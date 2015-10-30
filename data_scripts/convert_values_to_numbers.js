4f7c611b793b9a95948dfc79599fb3f2c2099ba5
Joe Flasher <joseph.flasher@gmail.com>
2015-10-30 14:59:53 -0400

    Make all value strings numbers, for real this time
    closes #1
    [ci skip]

--- data_scripts/convert_values_to_numbers.js
+++ data_scripts/convert_values_to_numbers.js
@@ -2,7 +2,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
 var dbURL = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/openAQ';
-var measurementsCollection;
 
 MongoClient.connect(dbURL, function (err, db) {
   if (err) {
@@ -10,17 +9,22 @@ MongoClient.connect(dbURL, function (err, db) {
   }
   console.info('Connected to database.');
 
-  // Get collection and ensure indexes
-  measurementsCollection = db.collection('measurements');
-  measurementsCollection.find({}).toArray(function (e, ms) {
-    for (var i = 0; i < ms.length; i++) {
-      var m = ms[i];
-      if (typeof m.value !== 'number') {
-        console.info('Saving new value: ', i + 1);
-        m.value = Number(m.value);
-        measurementsCollection.save(m);
+  // Find values that are stored as string and convert to numbers
+  var measurementsCollection = db.collection('measurements');
+  var bulk = measurementsCollection.initializeUnorderedBulkOp();
+  measurementsCollection.find({value: {$type: 2}}).toArray(function (e, ms) {
+    ms.forEach(function (m) {
+      var number = Number(m.value);
+      bulk.find({_id: m._id}).updateOne({$set: {value: number}});
+    });
+    bulk.execute(function (err, result) {
+      if (err) {
+        console.error(err);
+        return db.close();
       }
-    }
-    db.close();
+
+      console.info('Measurements updated:', result.nModified);
+      return db.close();
+    });
   });
 });
