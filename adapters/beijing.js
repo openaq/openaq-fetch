16638a17410241159de85f20e5e3bb8e0917a4d3
Dolugen Buuralda <dolugen@gmail.com>
2016-02-22 13:28:09 +0800

    Update adapter and source for China

--- adapters/beijing.js
+++ adapters/beijing.js
@@ -1,14 +1,13 @@
 /**
  * This code is responsible for implementing all methods related to fetching
- * and returning data for the Beijing Kimono data source.
- *
- * @todo This should most likely be moved to a new, consolidated adapter in the future.
+ * and returning data from StateAir.net data source.
  */
 'use strict';
 
 var request = require('request');
 var _ = require('lodash');
 var moment = require('moment-timezone');
+var cheerio = require('cheerio');
 var log = require('../lib/logger');
 
 exports.name = 'beijing';
@@ -17,10 +16,10 @@ exports.name = 'beijing';
  * Fetches the data for a given source and returns an appropriate object
  * @param {object} source A valid source object
  * @param {function} cb A callback of the form cb(err, data)
- */
+ * @return {object} Parsed and standardized data our system can use
+ */ 
 exports.fetchData = function (source, cb) {
-  var finalURL = source.url + '?apitoken=' + process.env.INDIA_KIMONO_TOKEN;
-  request(finalURL, function (err, res, body) {
+  request(source.url, function (err, res, body) {
     if (err || res.statusCode !== 200) {
       log.error(err || res);
       return cb({message: 'Failure to load data url.'});
@@ -29,7 +28,7 @@ exports.fetchData = function (source, cb) {
     // Wrap everything in a try/catch in case something goes wrong
     try {
       // Format the data
-      var data = formatData(body);
+      var data = formatData(body, source);
 
       // Make sure the data is valid
       if (data === undefined) {
@@ -45,30 +44,9 @@ exports.fetchData = function (source, cb) {
 /**
  * Given fetched data, turn it into a format our system can use.
  * @param {object} data Fetched source data
- * @return {object} Parsed and standarized data our system can use
+ * @param {object} source A valid source object
  */
-var formatData = function (data) {
-  // Wrap the JSON.parse() in a try/catch in case it fails
-  try {
-    data = JSON.parse(data);
-  } catch (e) {
-    // Return undefined to be caught elsewhere
-    return undefined;
-  }
-
-  /**
-   * Turns the source value strings into something useable by the system.
-   * @param {string} measuredValue Value string from source
-   * @return {object} An object containing value and unit for measure value
-   */
-  var getValue = function (measuredValue) {
-    var idx = measuredValue.indexOf('\n');
-    var idx2 = measuredValue.indexOf(' ', idx);
-    return {
-      value: measuredValue.substring(idx + 1, idx2),
-      unit: measuredValue.substring(idx2 + 1, measuredValue.length)
-    };
-  };
+var formatData = function (data, source) {
 
   /**
    * Given a date string, convert to system appropriate times.
@@ -76,33 +54,41 @@ var formatData = function (data) {
    * @return {object} An object containing both UTC and local times
    */
   var getDate = function (dateString) {
-    var date = moment.tz(dateString, 'MMM DD, YYYY h A', 'Asia/Shanghai');
-
+    var date = moment.tz(dateString, "MM/DD/YYYY HH:mm:ss A", "Asia/Shanghai");
     return {utc: date.toDate(), local: date.format()};
   };
 
-  // Filter out measurements with no value
-  var filtered = _.filter(data.results.collection1, function (m) {
-    return getValue(m.measuredValue).value !== '';
-  });
+  // Load all the XML
+  var $ = cheerio.load(data, {xmlMode: true});
 
-  // Build up pretty measurements array
-  var measurements = _.map(filtered, function (m) {
-    var valueObj = getValue(m.measuredValue);
+  // Create measurements array
+  var measurements = [];
 
-    // Manually adding offset, find a better way to do this
-    var date = getDate(m.date);
-    return {
-      parameter: 'pm25',
-      date: date,
-      value: Number(valueObj.value),
-      unit: valueObj.unit
-    };
-  });
-  var parsed = {
-    'name': data.name,
-    'measurements': measurements
+  // Build up the base object
+  var baseObj = {
+    location: source.name,
+    parameter: 'pm25',
+    unit: 'µg/m³',
+    averagingPeriod: {'value': 1, 'unit': 'hours'},
+    attribution: [{
+      name: 'EPA AirNow DOS',
+      url: 'http://airnow.gov/index.cfm?action=airnow.global_summary'
+    }]
   };
 
-  return parsed;
+  // Loop over each item and save the object
+  $('item').each(function (i, elem) {
+    // Clone base object
+    var obj = _.cloneDeep(baseObj);
+
+    obj.value = Number($(elem).children('Conc').text());
+    obj.date = getDate($(elem).children('ReadingDateTime').text());
+
+    measurements.push(obj);
+  });
+
+  return {
+    name: 'unused',
+    measurements: measurements
+  };
 };
