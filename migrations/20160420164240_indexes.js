3a4ea409a11a1c3d8b6d209ac852119e1ed30761
Joe Flasher <joseph.flasher@gmail.com>
2016-04-21 11:15:22 -0400

    Adding a new index to better handle city/country queries

---
+++ migrations/20160420164240_indexes.js
@@ -0,0 +1,24 @@
+/**
+ * Creates a better index for the cities/countries aggregation
+ */
+
+'use strict';
+
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.raw('create index measurements_country_location_city_idx on measurements (country, location, city);')
+  ])
+  .catch((e) => {
+    // Since this takes a lot of time, we'll want to run concurrently on the
+    // live db, which we can't do here. So the index may already be created
+    // when this runs, so catch the error to be nice.
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.table('measurements', (table) => {
+      table.dropIndex(['country', 'location', 'city']);
+    })
+  ]);
+};
