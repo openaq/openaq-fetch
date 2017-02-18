d4926f512251037ed459dc3648efd404a0c69b19
Dolugen Buuralda <dolugen@users.noreply.github.com>
2017-02-19 04:10:28 +0800

    Expect different formatting of PM2.5 for CPCB

--- adapters/cpcb.js
+++ adapters/cpcb.js
@@ -161,10 +161,12 @@ const renameParameter = function (parameter) {
     case 'Carbon Monoxide':
       return 'co';
     case 'PM2.5':
+    case 'PM 2.5':
       return 'pm25';
     case 'Ozone':
       return 'o3';
     case 'PM10':
+    case 'PM 10':
       return 'pm10';
     default:
       return parameter;
