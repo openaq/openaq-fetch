fae8a0938cae6bda01fec0c7a8e53e4a6428a331
Dolugen Buuralda <dolugen@gmail.com>
2017-07-04 11:55:37 +0800

    Fix other docker-compose -p calls

--- .build_scripts/build.sh
+++ .build_scripts/build.sh
@@ -10,7 +10,7 @@ echo No build currently needed since we can not cache because we are on Travis l
 # fi
 
 # touch local.env
-# docker-compose --project openaq build
+# docker-compose --project-name openaq build
 
 # mkdir -p $HOME/docker
 # echo "Caching openaq_fetch docker image."
