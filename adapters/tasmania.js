9e048947e124ef13530eb60951d04f7f9556780d
Bryce <bryce.esonica@gmail.com>
2017-07-10 19:29:30 +1000

    more formatting *sigh

--- adapters/tasmania.js
+++ adapters/tasmania.js
@@ -44,52 +44,51 @@ var formatData = function (data) {
   // http://epa.tas.gov.au/_layouts/15/Lightbox.aspx?url=http%3A%2F%2Fepa.tas.gov.au%2FAir%2FLive%2Flatest_air_data_on_map.jpg
 
   var stations = {
-    'ST':'Smithton',
-    'WY':'Wynyard',
-    'ER':'Emu River',
-    'WU':'West Ulverstone',
-    'DT':'Devonport',
-    'SF':'Sheffield',
-    'DL':'Deloraine',
-    'WE':'Westbury',
-    'HA':'Hadspen',
-    'LF':'Longford',
-    'PE':'Perth',
-    'GB':'George Town',
-    'EX':'Exeter',
-    'TI':'Ti Tree Bend',
-    'SL':'South Launceston',
-    'LD':'Lilydale',
-    'SC':'Scottsdale',
-    'DE':'Derby',
-    'SH':'St Helens',
-    'FI':'Fingal',
-    'PO':'Poatina',
-    'CT':'Campbell Town',
-    'OL':'Oatlands',
-    'TR':'Triabunna',
-    'BC':'Bream Creek',
-    'GR':'Gretna',
-    'NN':'New Norfolk',
-    'GO':'Glenorchy',
-    'HT':'Hobart',
-    'MT':'Mornington',
-    'JB':'Judbury',
-    'HV':'Huonville',
-    'CY':'Cygnet',
-    'GV':'Geeveston'
+    'ST': 'Smithton',
+    'WY': 'Wynyard',
+    'ER': 'Emu River',
+    'WU': 'West Ulverstone',
+    'DT': 'Devonport',
+    'SF': 'Sheffield',
+    'DL': 'Deloraine',
+    'WE': 'Westbury',
+    'HA': 'Hadspen',
+    'LF': 'Longford',
+    'PE': 'Perth',
+    'GB': 'George Town',
+    'EX': 'Exeter',
+    'TI': 'Ti Tree Bend',
+    'SL': 'South Launceston',
+    'LD': 'Lilydale',
+    'SC': 'Scottsdale',
+    'DE': 'Derby',
+    'SH': 'St Helens',
+    'FI': 'Fingal',
+    'PO': 'Poatina',
+    'CT': 'Campbell Town',
+    'OL': 'Oatlands',
+    'TR': 'Triabunna',
+    'BC': 'Bream Creek',
+    'GR': 'Gretna',
+    'NN': 'New Norfolk',
+    'GO': 'Glenorchy',
+    'HT': 'Hobart',
+    'MT': 'Mornington',
+    'JB': 'Judbury',
+    'HV': 'Huonville',
+    'CY': 'Cygnet',
+    'GV': 'Geeveston'
   };
 
   var output = [];
   var measurements = [];
 
   // parse the csv feed, exclude # lines
-  var output = parse(data, {trim: true, comment: '#'});
+  output = parse(data, {trim: true, comment: '#'});
 
   // loop through the csv rows
-  for(var k = 0; k < output.length; k++)
-  {
-    //Station, hhmmss(AEST), PM2.5(ug/m^3), PM10(ug/m^3), lat(deg), long(degE), alt(m)
+  for (var k = 0; k < output.length; k++) {
+    // Station, hhmmss(AEST), PM2.5(ug/m^3), PM10(ug/m^3), lat(deg), long(degE), alt(m)
     var value = output[k];
     var currentDate = value[1];
 
@@ -103,7 +102,6 @@ var formatData = function (data) {
       var pm10 = value[3];
       var lat = value[4];
       var lng = value[5];
-      var alt = value[6];
 
       // PM2.5 entry
       var m = {
@@ -112,13 +110,13 @@ var formatData = function (data) {
         parameter: 'pm25',
         date: dates,
         coordinates: {
-        latitude: Number(lat),
-        longitude: Number(lng)
-      },
-      value: Number(pm25),
-      unit: 'µg/m³',
-      attribution: [{name: 'Environmental Protection Authority - Tasmania', url: 'http://epa.tas.gov.au'}],
-      averagingPeriod: {unit: 'minutes', value: 10}
+          latitude: Number(lat),
+          longitude: Number(lng)
+        },
+        value: Number(pm25),
+        unit: 'µg/m³',
+        attribution: [{name: 'Environmental Protection Authority - Tasmania', url: 'http://epa.tas.gov.au'}],
+        averagingPeriod: {unit: 'minutes', value: 10}
       };
       measurements.push(m);
 
@@ -129,22 +127,20 @@ var formatData = function (data) {
         parameter: 'pm10',
         date: dates,
         coordinates: {
-        latitude: Number(lat),
-        longitude: Number(lng)
-      },
-      value: Number(pm10),
-      unit: 'µg/m³',
-      attribution: [{name: 'Environmental Protection Authority - Tasmania', url: 'http://epa.tas.gov.au'}],
-      averagingPeriod: {unit: 'minutes', value: 10}
+          latitude: Number(lat),
+          longitude: Number(lng)
+        },
+        value: Number(pm10),
+        unit: 'µg/m³',
+        attribution: [{name: 'Environmental Protection Authority - Tasmania', url: 'http://epa.tas.gov.au'}],
+        averagingPeriod: {unit: 'minutes', value: 10}
       };
       measurements.push(p);
     }
   }
   measurements = convertUnits(_.flatten(measurements));
   return {
-  name: 'unused',
-  measurements: measurements
+    name: 'unused',
+    measurements: measurements
   };
 };
-
-
