55be8e465b95b55e250265bb51bb1726dd311212
Dolugen Buuralda <dolugen@gmail.com>
2017-07-16 00:09:46 +0800

    It wasn't timing out after all

--- adapters/envista_za.js
+++ adapters/envista_za.js
@@ -2,7 +2,7 @@
 
 import { default as baseRequest } from 'request';
 import { REQUEST_TIMEOUT } from '../lib/constants';
-const request = baseRequest.defaults({timeout: REQUEST_TIMEOUT * 3});
+const request = baseRequest.defaults({timeout: REQUEST_TIMEOUT});
 import cheerio from 'cheerio';
 import { default as moment } from 'moment-timezone';
 import { flattenDeep, isFinite } from 'lodash';
