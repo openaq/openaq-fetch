bbf2449548ff0ecf70df663c986817ef01882b7d
maxgrossman <m.grossman1813@gmail.com>
2017-07-19 17:56:55 -0400

    removed problem attribute
    the name attribute in one of the objs in stations was causing the linting issue
    since these names are not used in the adapter I got rid of each in list of objects

--- adapters/campania.js
+++ adapters/campania.js
@@ -90,218 +90,182 @@ const stations = [
   {
     'ID': 'ARPACAMPANIA_ACERRAZI',
     'LAT': 40.9796485901,
-    'LON': 14.4009828568,
-    'NAME': 'Acerra Zona Industriale'
+    'LON': 14.4009828568
   },
   {
     'ID': 'ARPACAMPANIA_ALBURNI',
     'LAT': 40.470664978,
-    'LON': 15.2986392975,
-    'NAME': 'Ottati Alburni'
+    'LON': 15.2986392975
   },
   {
     'ID': 'ARPACAMPANIA_ALIGHIERI',
     'LAT': 40.9187049866,
-    'LON': 14.7854919434,
-    'NAME': 'Avellino Scuola Alighieri'
+    'LON': 14.7854919434
   },
   {
     'ID': 'ARPACAMPANIA_AREAASI',
     'LAT': 40.9208335876,
-    'LON': 14.3850002289,
-    'NAME': `Pomigliano D'Arco Area ASI`
+    'LON': 14.3850002289
   },
   {
     'ID': 'ARPACAMPANIA_AV41',
     'LAT': 40.9230537415,
-    'LON': 14.7866668701,
-    'NAME': 'Avellino AV41 Sc. V Circolo'
+    'LON': 14.7866668701
   },
   {
     'ID': 'ARPACAMPANIA_BENEVCS',
     'LAT': 41.1157493591,
-    'LON': 14.7798948288,
-    'NAME': 'Benevento Campo Sportivo'
+    'LON': 14.7798948288
   },
   {
     'ID': 'ARPACAMPANIA_BENEVZI',
     'LAT': 41.1468429565,
-    'LON': 14.8362503052,
-    'NAME': 'Benevento Zona Industriale'
+    'LON': 14.8362503052
   },
   {
     'ID': 'ARPACAMPANIA_BN32',
     'LAT': 41.1313896179,
-    'LON': 14.7891664505,
-    'NAME': 'Benevento BN32 Via Mustilli'
+    'LON': 14.7891664505
   },
   {
     'ID': 'ARPACAMPANIA_CAPORALE',
     'LAT': 40.9404602051,
-    'LON': 14.3701972961,
-    'NAME': 'Acerra Scuola Caporale'
+    'LON': 14.3701972961
   },
   {
     'ID': 'ARPACAMPANIA_CASORIA',
     'LAT': 40.9146308899,
-    'LON': 14.2986059189,
-    'NAME': 'Casoria Scuola Palizzi'
+    'LON': 14.2986059189
   },
   {
     'ID': 'ARPACAMPANIA_CE51',
     'LAT': 41.0786094666,
-    'LON': 14.3383331299,
-    'NAME': 'Caserta CE51 Ist.Manzoni'
+    'LON': 14.3383331299
   },
   {
     'ID': 'ARPACAMPANIA_CE52',
     'LAT': 41.076915741,
-    'LON': 14.3312501907,
-    'NAME': 'Caserta CE52 Sc. De Amicis'
+    'LON': 14.3312501907
   },
   {
     'ID': 'ARPACAMPANIA_CE54',
     'LAT': 41.0459289551,
-    'LON': 14.3780832291,
-    'NAME': 'Maddaloni CE54 Sc. Settembrini'
+    'LON': 14.3780832291
   },
   {
     'ID': 'ARPACAMPANIA_CIRILLO',
     'LAT': 40.9754981995,
-    'LON': 14.2114162445,
-    'NAME': 'Aversa Sc. Cirillo'
+    'LON': 14.2114162445
   },
   {
     'ID': 'ARPACAMPANIA_CS',
     'LAT': 40.9904327393,
-    'LON': 14.423989296,
-    'NAME': 'S. Felice a Cancello C. Scolast.'
+    'LON': 14.423989296
   },
   {
     'ID': 'ARPACAMPANIA_MARCONI',
     'LAT': 40.9244270325,
-    'LON': 14.4814472198,
-    'NAME': 'S.Vitaliano Scuola Marconi'
+    'LON': 14.4814472198
   },
   {
     'ID': 'ARPACAMPANIA_MATESE',
     'LAT': 41.4183616638,
-    'LON': 14.412027359,
-    'NAME': 'S.Gregorio Matese Lago'
+    'LON': 14.412027359
   },
   {
     'ID': 'ARPACAMPANIA_MERC',
     'LAT': 40.6616401672,
-    'LON': 14.8045139313,
-    'NAME': 'Salerno Parco Mercatello'
+    'LON': 14.8045139313
   },
   {
     'ID': 'ARPACAMPANIA_NA01',
     'LAT': 40.8636932373,
-    'LON': 14.2545108795,
-    'NAME': 'Napoli NA01 Oss. Astronomico'
+    'LON': 14.2545108795
   },
   {
     'ID': 'ARPACAMPANIA_NA02',
     'LAT': 40.8491668701,
-    'LON': 14.2311115265,
-    'NAME': 'Napoli NA02 Osp. Santobono'
+    'LON': 14.2311115265
   },
   {
     'ID': 'ARPACAMPANIA_NA06',
     'LAT': 40.8541679382,
-    'LON': 14.2511110306,
-    'NAME': 'Napoli NA06 Museo Nazionale'
+    'LON': 14.2511110306
   },
   {
     'ID': 'ARPACAMPANIA_NA07',
     'LAT': 40.8541679382,
-    'LON': 14.2716665268,
-    'NAME': 'Napoli NA07 Ferrovia'
+    'LON': 14.2716665268
   },
   {
     'ID': 'ARPACAMPANIA_NA08',
     'LAT': 40.8725013733,
-    'LON': 14.2827777863,
-    'NAME': 'Napoli NA08 Osp.N.Pellegrini'
+    'LON': 14.2827777863
   },
   {
     'ID': 'ARPACAMPANIA_NA09',
     'LAT': 40.8638877869,
-    'LON': 14.3413887024,
-    'NAME': 'Napoli NA09 Via Argine'
+    'LON': 14.3413887024
   },
   {
     'ID': 'ARPACAMPANIA_NA10',
     'LAT': 40.7986679077,
-    'LON': 14.1797218323,
-    'NAME': 'Napoli Parco Virgiliano'
+    'LON': 14.1797218323
   },
   {
     'ID': 'ARPACAMPANIA_PARCOF',
     'LAT': 40.6110839844,
-    'LON': 14.9758615494,
-    'NAME': 'Battipaglia Parco Fiume'
+    'LON': 14.9758615494
+
   },
   {
     'ID': 'ARPACAMPANIA_PASCOLI',
     'LAT': 40.7604980469,
-    'LON': 14.4381332397,
-    'NAME': 'Torre Annunziata Sc. Pascoli'
+    'LON': 14.4381332397
   },
   {
     'ID': 'ARPACAMPANIA_POLLA',
     'LAT': 40.4880828857,
-    'LON': 15.5200834274,
-    'NAME': 'Polla Area Ritovagliatore'
+    'LON': 15.5200834274
   },
   {
     'ID': 'ARPACAMPANIA_PR',
     'LAT': 40.8159713745,
-    'LON': 14.3502502441,
-    'NAME': 'Portici Parco Reggia'
+    'LON': 14.3502502441
   },
   {
     'ID': 'ARPACAMPANIA_SA22',
     'LAT': 40.682220459,
-    'LON': 14.7661113739,
-    'NAME': 'Salerno SA22 Osp. Via Vernieri'
+    'LON': 14.7661113739
   },
   {
     'ID': 'ARPACAMPANIA_SA23',
     'LAT': 40.6952781677,
-    'LON': 14.7775001526,
-    'NAME': 'Salerno SA23 Scuola Conti'
+    'LON': 14.7775001526
   },
   {
     'ID': 'ARPACAMPANIA_SOLIMENA',
     'LAT': 40.7402496338,
-    'LON': 14.6434669495,
-    'NAME': 'Nocera Inferiore Sc. Solimena'
+    'LON': 14.6434669495
   },
   {
     'ID': 'ARPACAMPANIA_SOLOFRAZI',
     'LAT': 40.8355560303,
-    'LON': 14.8249998093,
-    'NAME': 'Solofra Zona Industriale'
+    'LON': 14.8249998093
   },
   {
     'ID': 'ARPACAMPANIA_STADIO',
     'LAT': 40.710975647,
-    'LON': 14.7024440765,
-    'NAME': 'Cava dei Tirreni Stadio'
+    'LON': 14.7024440765
   },
   {
     'ID': 'ARPACAMPANIA_VILLAAV',
     'LAT': 40.8228759766,
-    'LON': 14.1224365234,
-    'NAME': 'Pozzuoli Zona Villa Avellino'
+    'LON': 14.1224365234
   },
   {
     'ID': 'ARPACAMPANIA_VILLACOM',
     'LAT': 41.1563072205,
-    'LON': 15.0965003967,
-    'NAME': 'Ariano Irpino Stadio'
+    'LON': 15.0965003967
   }
 ];
-
