ef9ff5548e5faa6656ae04e23faaed91351bdc17
Dolugen Buuralda <dolugen@gmail.com>
2017-07-23 00:59:10 +0800

    Async flow

--- adapters/arpalazio.js
+++ adapters/arpalazio.js
@@ -7,99 +7,139 @@ import { default as moment } from 'moment-timezone';
 import cheerio from 'cheerio';
 import { parallel } from 'async';
 import { acceptableParameters, convertUnits } from '../lib/utils';
-import { default as parse} from 'csv-parse/lib/sync';
-import { zip } from 'lodash';
+import { default as parse } from 'csv-parse/lib/sync';
+import { zip, flattenDeep } from 'lodash';
 
 exports.name = 'arpalazio';
 
+const baseUrl = 'http://www.arpalazio.net/main/aria/sci/annoincorso/';
+const provinceQueryPath = 'chimici/chimici.php';
+
 exports.fetchData = function (source, cb) {
-  const baseUrl = 'http://www.arpalazio.net/main/aria/sci/annoincorso/';
-  const provinceQueryPath = 'chimici/chimici.php';
   let datiOrari = 0;
   request(source.url, (err, res, body) => {
     if (err || res.statusCode !== 200) {
-      console.error(err);
-      process.exit();
+      cb(err || res);
     }
 
     const $ = cheerio.load(body);
     let provinces = $('#provincia option')
         .filter(function (i, el) {
-          return Number($(this).attr('value')) >= 0
+          return Number($(this).attr('value')) >= 0;
         })
-        .map(function(i, el) {
-          return { id: $(this).attr('value'), name: $(this).text() }
+        .map(function (i, el) {
+          return { id: $(this).attr('value'), name: $(this).text() };
         }).get();
-    console.log(provinces);
-    
+
+    let tasks = [];
     provinces.forEach(function (province) {
       const provinceURL = `${baseUrl}${provinceQueryPath}?provincia=${province.id}&dati=${datiOrari}`;
-      request(provinceURL, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error(err);
-          process.exit();
-        }
-        // get pollutant.txt links
-        const $ = cheerio.load(body);
-        let pollutantURLs = $('a').map(function (i, el) {
-          const pollutant = $(this).text().toLowerCase().replace('.', '');
-          if (acceptableParameters.indexOf(pollutant) >= 0) {
-            const href = $(this).attr('href');
-            return `${baseUrl}${href}`;            
-          }
-        }).get();
-        // retrieve .txt files
-        console.log(pollutantURLs);
-        //pollutantURLs = pollutantURLs.slice(0, 1);
-        pollutantURLs.forEach(function (url) {
-          //determine year, pollutant from url
-          console.log(url);
-          const match = url.match(/[\w]{2}_([\w\.]{2,5})_([\d]{4}).txt/);
-          const parameter = match[1].toLowerCase().replace('.', '');
-          const year = match[2];
-          
-          request(url, (err, res, body) => {
-            // remove whitespace for clean parsing
-            body = body.replace(/[ ]+/g, ' ');
-            body = body.replace(/^[ ]/g, '');
-            body = body.replace(/\n[ ]+/g, '\n');
-            
-            const parsed = parse(body, {delimiter: ' '});
-            const headers = parsed.slice(0, 1)[0];
-            const records = parsed.slice(1);
+      tasks.push(handleProvince(provinceURL, province.name, source));
+    });
 
-            // zip them to check -999 values
-            let zipped = zip(...records);
-            let invalidStationIDs = [];
-            zipped.forEach(function (values, i, _) {
-              if (values.every((v) => Number(v) === -999)) {
-                invalidStationIDs.push(headers[i]);
-              }
-            });
-            //console.log(headers);
-            //console.log(records.slice(0, 10));
-            records.slice(0, 10).forEach(function(row) {
-              const date = moment.tz(`${year} ${row[0]} ${row[1]}`, "YYYY DDD HH", "Europe/Rome");
-              const rowValuesOffset = 2;
-              row.slice(rowValuesOffset).forEach(function (value, i, _) {
-                if (invalidStationIDs.indexOf(headers[i+rowValuesOffset]) >= 0) {
-                  return;
-                }
-                const location = 'Location ' + headers[i+2];
-                let m = {
-                  date: date.format(),
-                  location: location,
-                  value: Number(value),
-                  city: province.name,
-                  parameter: parameter
-                };
-                console.log(m);
-              })
-            })
-            //process.exit();
-          });
-        });
-      });
+    parallel(tasks, (err, results) => {
+      if (err) {
+        return cb(err, []);
+      }
+      results = flattenDeep(results);
+      results = convertUnits(results);
+
+      return cb(err, {name: 'unused', measurements: results});
     });
   });
-}
+};
+
+const handleProvince = function (url, name, source) {
+  return function (done) {
+    request(url, (err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        return done(null, []);
+      }
+      // get pollutant.txt links
+      const $ = cheerio.load(body);
+      let pollutantURLs = $('a').map(function (i, el) {
+        const pollutant = $(this).text().toLowerCase().replace('.', '');
+        if (acceptableParameters.indexOf(pollutant) >= 0) {
+          const href = $(this).attr('href');
+          return `${baseUrl}${href}`;
+        }
+      }).get();
+
+      let tasks = [];
+      console.log(pollutantURLs.length);
+      pollutantURLs.forEach((url) => tasks.push(getData(url, name, source)));
+      parallel(tasks, (err, results) => {
+        return done(err, results);
+      });
+    });
+  };
+};
+
+const getData = function (url, city, source) {
+  return function (done) {
+    const match = url.match(/[\w]{2}_([\w\.]{2,})_([\d]{4}).txt/);
+    const parameter = match[1].toLowerCase().replace('.', '');
+    const year = match[2];
+
+    request(url, (err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        console.log(url);
+        return done(null, []);
+      }
+      // remove whitespace for clean parsing
+      body = body.replace(/[ ]+/g, ' ');
+      body = body.replace(/^[ ]/g, '');
+      body = body.replace(/\n[ ]+/g, '\n');
+
+      const parsed = parse(body, {delimiter: ' '});
+      const headers = parsed.slice(0, 1)[0];
+      const records = parsed.slice(1);
+
+      // zip them to check -999 values
+      let zipped = zip(...records);
+      let invalidStationIDs = [];
+      zipped.forEach(function (values, i, _) {
+        if (values.every((v) => Number(v) === -999)) {
+          invalidStationIDs.push(headers[i]);
+        }
+      });
+
+      let measurements = [];
+      records.forEach(function (row) {
+        const date = moment.tz(`${year} ${row[0]} ${row[1]}`, 'YYYY DDD HH', 'Europe/Rome');
+        const fullDate = {
+          utc: date.toDate(),
+          local: date.format()
+        };
+        const rowValuesOffset = 2; // skip the date parts
+
+        let base = {
+          date: fullDate,
+          city: city,
+          averagingPeriod: {unit: 'hours', value: 1},
+          attribution: [{
+            name: source.name,
+            url: source.sourceURL
+          }]
+        };
+
+        row.slice(rowValuesOffset).forEach(function (value, i, _) {
+          if (invalidStationIDs.indexOf(headers[i + rowValuesOffset]) >= 0) {
+            return;
+          }
+          const location = 'Location ' + headers[i + rowValuesOffset]; // FIXME
+
+          let m = Object.assign({}, base);
+          m.location = location;
+          m.value = Number(value);
+          m.parameter = parameter;
+          m.unit = 'ppm'; // FIXME
+          // m.coordinates = {}; //FIXME
+
+          measurements.push(m);
+        });
+      });
+      done(null, measurements);
+    });
+  };
+};
