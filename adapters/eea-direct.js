c2b69b6b8b01e2b6d1f2cf84c58e239df263fb6e
maxgrossman <m.grossman1813@gmail.com>
2017-08-28 21:19:42 -0400

    refactor eea adapter to read directly from batutta output

--- adapters/eea-direct.js
+++ adapters/eea-direct.js
@@ -5,14 +5,11 @@ import { REQUEST_TIMEOUT } from '../lib/constants';
 import { default as baseRequest } from 'request';
 import { default as moment } from 'moment-timezone';
 import { parallel, map } from 'async';
-import { uniqBy } from 'lodash';
-import { default as parse } from 'csv-parse/lib/sync';
+import { uniq } from 'lodash';
 import tzlookup from 'tz-lookup';
-import bboxPolygon from '@turf/bbox-polygon';
-import inside from '@turf/inside';
-import { point } from '@turf/helpers';
+import { default as parse } from 'csv-parse/lib/sync';
 const request = baseRequest.defaults({timeout: REQUEST_TIMEOUT});
-const stationsLink = 'http://battuta.s3.amazonaws.com/eea-stations.json';
+const stationsLink = 'http://battuta.s3.amazonaws.com/eea-stations-all.json';
 
 export const name = 'eea-direct';
 
@@ -37,46 +34,21 @@ export function fetchData (source, cb) {
 const makeMetadataRequest = (source) => {
   return (cb) => {
     request.get({
-      url: 'http://discomap.eea.europa.eu/map/fme/metadata/PanEuropean_metadata.csv'
+      url: stationsLink
     }, (err, res, body) => {
       if (err || res.statusCode !== 200) {
         return cb('Could not gather current metadata, will generate records without coordinates.', []);
       }
       let data;
       try {
-        data = parse(body, {delimiter: '\t'});
+        data = JSON.parse(body);
       } catch (e) {
         return cb('Could not parse metadata file', []);
       }
-      getCoordinates(data, source.country, cb);
+      cb(null, data);
+      // getCoordinates(data, source.country, cb);
     });
   };
-};
-
-// reduce metadata to list of objects with coordinates for
-const getCoordinates = (metadata, country, callback) => {
-  // filter for only country of interest's records
-  metadata = metadata.filter((record) => {
-    return record[0] === country;
-  });
-  map(metadata, (record, done) => {
-    const lat = Number(record[15]);
-    const lon = Number(record[14]);
-    const station = {
-      stationId: record[5],
-      coordinates: {
-        latitude: lat,
-        longitude: lon
-      },
-      tz: tzlookup(lat, lon)
-    };
-    done(null, station);
-  }, (err, mappedRecords) => {
-    if (err) {
-      return callback(null, []);
-    }
-    callback(null, uniqBy(mappedRecords, 'stationId'));
-  });
 };
 
 // makes requests to get country's pollutant data.
@@ -91,7 +63,7 @@ const makeTaskRequests = (source) => {
         if (err || res.statusCode !== 200) {
           return done(null, []);
         }
-        done(null, parse(body).slice(1, -1));
+        done(null, parse(body, {relax_column_count: true}).slice(1, -1));
       });
     };
   });
@@ -110,142 +82,60 @@ const makeTaskRequests = (source) => {
 
 // formats data to match openAQ standard
 const formatData = (data, source, cb) => {
-  const coordinates = data[0];
+  const stations = data[0];
   const records = data[1];
-  parallel([
-    (done) => {
-      map(records, (record, cb) => {
-        const tz = getTZ(coordinates, record[11]);
-        let measurement = {
-          coordinates: makeCoordinates(coordinates, record[11]),
-          parameter: record[5].toLowerCase(),
-          date: makeDate(record[16], tz),
-          value: Number(record[19]),
-          unit: record[record.length - 1],
-          attribution: [{
-            name: 'EEA',
-            url: source.sourceUrl
-          }],
-          averagingPeriod: {
-            unit: 'hours',
-            value: 1
-          }
-        };
-        // apply unit conversion to generated record
-        cb(null, convertUnits([measurement])[0]);
-      }, (err, mappedRecords) => {
-        if (err) {
-          return done(null, []);
-        }
-        done(null, mappedRecords);
-      });
-    },
-    (done) => {
-      // read in location file, try to combine locations with coordinates
-      // by matching stationId or a spatial join, then return successful combos
-      // TODO: change this to fetch upon implementing new repo
-      request.get({url: stationsLink}, (err, res, locations) => {
-        if (err) {
-          return done(null, []);
-        }
-        try {
-          locations = JSON.parse(locations);
-        } catch (e) {
-          return done('Could not parse stations file', []);
-        }
-        map(coordinates, (crds, cb) => {
-          if (err) {
-            return cb(null, crds);
-          }
-          let geocodedProps = locations.find((location) => {
-            // an object with city, region, bounds, and stationId
-            // or just a stationId if reverse geocoding was unsuccessful
-            if (location.stationId) {
-              return location.stationId === crds.stationId;
-            } else {
-              return location === crds.stationId;
-            }
-          });
-          // if new station, denoted by no matching id,
-          // try to spatial join
-          if (!(geocodedProps)) {
-            geocodedProps = locations.find((location) => {
-              if (location.stationId) {
-                const bbox = bboxPolygon(location.bounds);
-                const stationPoint = point(
-                  crds.longitude,
-                  crds.latitude
-                );
-                return inside(stationPoint, bbox);
-              }
-            });
-            // if failed spatial selection, return original coords
-            if (!(geocodedProps)) {
-              return cb(null, crds);
-            }
-          }
-          if (geocodedProps) {
-            geocodedProps = typeof geocodedProps === 'string' ? crds : Object.assign(geocodedProps, crds);
-            cb(null, geocodedProps);
-          }
-        }, (err, updatedCoordinates) => {
-          if (err) {
-            return done(null, coordinates);
-          }
-          done(null, updatedCoordinates);
-        });
-      });
+  let missed = [];
+  map(records, (record, done) => {
+    const matchedStation = matchStation(stations, record[11]);
+    if (!(matchedStation)) {
+      missed.push(record[11]);
+      return done(null, {});
     }
-  ], (err, mappedData) => {
+    const timeZone = tzlookup(matchedStation.latitude, matchedStation.longitude);
+    const offsetString = moment().tz(timeZone).format('Z');
+    let m = {
+      location: matchedStation.location ? matchedStation.location : (
+        matchedStation.city ? matchedStation.city : source.location
+      ),
+      city: matchedStation.city ? matchedStation.city : (
+        matchedStation.location ? matchedStation.location : source.city
+      ),
+      coordinates: {
+        latitude: Number(matchedStation.latitude),
+        longitude: Number(matchedStation.longitude)
+      },
+      parameter: record[5].toLowerCase(),
+      date: makeDate(record[16], offsetString),
+      value: Number(record[19]),
+      unit: record[record.length - 1],
+      attribution: [{
+        name: 'EEA',
+        url: source.sourceURL
+      }],
+      averagingPeriod: {
+        unit: 'hours',
+        value: 1
+      }
+    };
+    // apply unit conversion to generated record
+    done(null, convertUnits([m])[0]);
+  }, (err, measurements) => {
+    missed = uniq(missed);
     if (err) {
-      return cb(err, []);
+      return cb(null, {name: 'unused', measurements: []});
     }
-    let measurements = mappedData[0];
-    const coordinates = mappedData[1];
-    // map coordinates and location names to measurements
-    map(measurements, (measurement, done) => {
-      let station = matchStation(coordinates, measurement['coordinates']);
-      if (station.location) {
-        measurement['location'] = station.location;
-      }
-      if (station.city) {
-        measurement['city'] = station.city;
-      }
-      done(null, measurement);
-    }, (err, finalMeasurements) => {
-      if (err) {
-        return cb(null, {name: 'unused', measurements: measurements});
-      }
-      cb(null, {name: 'unused', measurements: finalMeasurements});
-    });
+    cb(null, {name: 'unused', measurements: measurements});
   });
 };
 
-const matchStation = (coordinates, measurementCoords) => {
-  let station;
-  if (coordinates.length > 0) {
-    station = coordinates.find((crds) => {
-      return crds.coordinates === measurementCoords;
-    });
-    return station;
-  }
+const matchStation = (stations, stationId) => {
+  return stations.find((station) => {
+    return station.stationId === stationId;
+  });
 };
 
-const makeCoordinates = (coordinatesList, stationId) => {
-  return coordinatesList.find((coordinates) => {
-    return coordinates.stationId === stationId;
-  }).coordinates;
-};
-
-const getTZ = (coordinatesList, stationId) => {
-  return coordinatesList.find((coordinates) => {
-    return coordinates.stationId === stationId;
-  }).tz;
-};
-
-const makeDate = (date, tz) => {
-  date = date.match(/\+/) ? date.split(/\+/)[0] : date.split(/-/)[0];
-  date = moment.tz(date, 'YYYY-MM-DD HH:mm:ss', tz);
+const makeDate = (date, offsetString) => {
+  date = moment.utc(date, 'YYYY-MM-DD HH:mm:ss').utcOffset(offsetString, true);
   return {
     utc: date.toDate(),
     local: date.format()
