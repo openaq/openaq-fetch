500cf818353ee797a89855f1f0fc0e5ecd261d76
michal@vantage <cz@signicode.com>
2018-10-16 20:50:06 +0200

    Fix eslint, remove old adapter code, add cache for adapters.

--- adapters/airnow-ke.js
+++ adapters/airnow-ke.js
@@ -1,10 +1,8 @@
 'use strict';
 
 import log from '../lib/logger';
-import { find } from 'lodash';
 import { default as moment } from 'moment-timezone';
-import { convertUnits, acceptableParameters } from '../lib/utils';
-import { parallel } from 'async';
+import { acceptableParameters } from '../lib/utils';
 import { REQUEST_TIMEOUT } from '../lib/constants';
 import { default as baseRequest } from 'request';
 import { StringStream } from 'scramjet';
@@ -34,12 +32,15 @@ const convertCity = function (city) {
   return city.split(',')[0].trim();
 };
 
-exports.fetchStream = async function (source) {
-  const locationsUrl = `${source.url}airnow/today/monitoring_site_locations.dat`;
-  log.debug(`Fetching AirNow locations from "${locationsUrl}"`);
+// map of promises for each url (probably this will have just a single key)
+const _locationsStream = {};
 
-  const locations = await (
-    StringStream.from(request(locationsUrl))
+function getLocations (url) {
+  if (!_locationsStream[url]) {
+    const locationsUrl = `${url}airnow/today/monitoring_site_locations.dat`;
+    log.verbose(`Fetching AirNow locations from "${locationsUrl}"`);
+    _locationsStream[url] = StringStream
+      .from(request(locationsUrl))
       .lines('\n')
       .parse((s) => {
         s = s.split('|');
@@ -62,8 +63,14 @@ exports.fetchStream = async function (source) {
       })
       .accumulate((acc, city) => {
         acc[city.aqsid] = city;
-      }, {})
-  );
+      }, {});
+  }
+
+  return _locationsStream[url];
+}
+
+exports.fetchStream = async function (source) {
+  const locations = await getLocations(source.url);
 
   log.debug(`Got ${Object.keys(locations).length} locations.`);
 
@@ -79,13 +86,11 @@ exports.fetchStream = async function (source) {
       const station = locations[m[2]];
 
       if (!parameter) {
-        return;
-        // throw new MeasurementValidationError(source, `Cannot parse parameter ${m[5]}`, m);
+        throw new MeasurementValidationError(source, `Cannot parse parameter ${m[5]}`, m);
       }
 
       if (!station) {
-        return;
-        // throw new MeasurementValidationError(source, `Cannot find station`, m);
+        throw new MeasurementValidationError(source, `Cannot find station`, m);
       }
 
       return {
@@ -103,120 +108,4 @@ exports.fetchStream = async function (source) {
     })
     .filter(m => m && acceptableParameters.includes(m.parameter))
   ;
-};
-
-exports.fetchData = function (source, cb) {
-  // Fetch locations and latest measurements in parallel
-  parallel({
-    stations: (done) => {
-      const url = `${source.url}airnow/today/monitoring_site_locations.dat`;
-      return request(url, (err, res, body) => {
-        return done(err, body);
-      });
-    },
-    measurements: (done) => {
-      // Filename should be the current time in UTC like '2016030616' and then
-      // get the previous hours measurements
-      const dateString = moment.utc().subtract(1, 'hours').format('YYYYMMDDHH');
-      const url = `${source.url}airnow/today/HourlyData_${dateString}.dat`;
-      return request(url, (err, res, body) => {
-        return done(err, body);
-      });
-    }
-  }, (err, results) => {
-    if (err) {
-      return cb({message: 'Failure to load data urls.'});
-    }
-
-    // Format the data
-    try {
-      const data = formatData(results);
-      // Make sure the data is valid
-      if (data === undefined) {
-        return cb({message: 'Failure to parse data.'});
-      }
-
-      return cb(null, data);
-    } catch (e) {
-      return cb({message: 'Unknown adapter error.'});
-    }
-  });
-};
-
-// Loop over measurements, adding station data and saving to database.
-const formatData = (results) => {
-  // Fancy up the locations object
-  results.stations = results.stations.split('\n');
-  results.stations = results.stations.map((s) => {
-    s = s.split('|');
-    return {
-      aqsid: s[0],
-      coordinates: {
-        latitude: Number(s[8]),
-        longitude: Number(s[9])
-      },
-      country: s[12],
-      city: convertCity(s[16]) || s[20]
-    };
-  });
-
-  // Special bit to account for Kenyan data being shared differently
-  // TODO: Fix this hopefully at the source?
-  results.stations = results.stations.map((s) => {
-    if (s.aqsid.slice(0, 3) === 'NRB') {
-      s.country = 'KE';
-      s.city = 'Nairobi';
-    }
-
-    return s;
-  });
-
-  // Fancy up the measurements object
-  results.measurements = results.measurements.split('\n');
-
-  // Filter out measurements that don't have a matching station
-  results.measurements = results.measurements.filter((m) => {
-    m = m.split('|');
-    return find(results.stations, {aqsid: m[2]});
-  });
-
-  // Generate measurement object
-  results.measurements = results.measurements.map((m) => {
-    m = m.split('|');
-    let parameter = m[5].toLowerCase().replace('.', '');
-    const station = find(results.stations, {aqsid: m[2]});
-    return {
-      coordinates: station.coordinates,
-      city: station.city,
-      country: station.country,
-      location: m[3].trim(),
-      date: getDate(m[0], m[1], Number(m[4])),
-      parameter: (parameter === 'ozone') ? 'o3' : parameter,
-      unit: m[6],
-      value: Number(m[7]),
-      attribution: [{name: 'US EPA AirNow', url: 'http://www.airnow.gov/'}, {name: m[8].trim()}],
-      averagingPeriod: {unit: 'hours', value: 1}
-    };
-  });
-
-  // We only need Kenya data for now
-  // results.measurements = results.measurements.filter((m) => {
-  //   return m.country === 'KE';
-  // });
-
-  // Filter out unwanted measurements
-  results.measurements = results.measurements.filter((m) => {
-    return acceptableParameters.includes(m.parameter);
-  });
-
-  // Final quality check, get rid of bad city and countries
-  results.measurements = results.measurements.filter((m) => {
-    return (m.city && m.city !== 'N/A' && m.country && m.country.trim() !== '');
-  });
-
-  // Convert units to platform standard
-  results.measurements = convertUnits(results.measurements);
-
-  // Ship it off to be saved!
-  return {name: 'unused', measurements: results.measurements};
 };
