d5639a9f11030e7234703aa765cffb27974990b8
jflasher <joseph.flasher@gmail.com>
2019-06-02 08:21:47 -0400

    Linting!

--- migrations/20190430111626_add-cities.js
+++ migrations/20190430111626_add-cities.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('cities', function (t) {
     t.string('country').notNullable();
     t.string('name').notNullable();
-    t.integer('count');     // measurements count
+    t.integer('count'); // measurements count
     t.integer('locations'); // locations count
     t.unique(['country', 'name']);
   });
