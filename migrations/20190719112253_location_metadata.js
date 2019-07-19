b97a33b1420556c0eb801083bb804a2f0df9db32
sethvincent <sethvincent@gmail.com>
2019-07-19 11:24:34 -0700

    reduce metadata migrations to one file

---
+++ migrations/20190719112253_location_metadata.js
@@ -0,0 +1,52 @@
+exports.up = function (knex) {
+  const createLocationsMetadata = knex.schema.createTable('locations_metadata', function (table) {
+    table.increments('id').primary();
+    table.string('locationId').notNullable();
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+    table.string('userId');
+    table.jsonb('data');
+    table.float('completeness');
+    table.index('locationId');
+  });
+
+  const createLatestLocationsView = knex.schema.raw(`
+    CREATE VIEW latest_locations_metadata AS
+    SELECT
+      locations_metadata.id,
+      locations_metadata."locationId",
+      locations_metadata."userId",
+      locations_metadata.data,
+      locations_metadata.completeness,
+      lm."createdAt",
+      lm."updatedAt",
+      lm.version
+    FROM locations_metadata
+    INNER JOIN (
+      SELECT
+        "locationId",
+        max("createdAt") as "updatedAt",
+        min("createdAt") as "createdAt",
+        COUNT("locationId") as version
+      FROM locations_metadata
+      GROUP BY "locationId"
+    ) lm ON locations_metadata."locationId" = lm."locationId" AND locations_metadata."createdAt" = lm."updatedAt"
+  `);
+
+  return Promise.all([
+    createLocationsMetadata,
+    createLatestLocationsView
+  ]);
+};
+
+exports.down = function (knex) {
+  const dropLocationsMetadata = knex.schema.dropTable('locations_metadata');
+
+  const dropLatestLocationsView = knex.schema.raw(`
+    DROP VIEW IF EXISTS latest_locations_metadata
+  `);
+
+  return Promise.all([
+    dropLocationsMetadata,
+    dropLatestLocationsView
+  ]);
+};
