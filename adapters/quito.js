430302203df15c5d370afb2b95095b085036bb53
Sruti <sruti@openaq.org>
2020-12-29 00:23:45 -0500

    Modify quito adapter to get all params

--- adapters/quito.js
+++ adapters/quito.js
@@ -1,139 +1,76 @@
 'use strict';
 
-import { REQUEST_TIMEOUT } from '../lib/constants';
-import { default as baseRequest } from 'request';
 import { default as moment } from 'moment-timezone';
+import _ from 'lodash';
+import log from '../lib/logger';
+import { promiseRequest, unifyParameters } from '../lib/utils';
 
-exports.name = 'quito';
+export const name = 'quito';
 
-const locations = [
-  'CML',
-  'COT',
-  'GUA',
-  'TUM',
-  'CH',
-  'P',
-  'ITC',
-  'CAR',
-  'JIP',
-  'BEL',
-  'CEN'
-];
-const measurementTypes = ['PM 2.5', 'PM 10', 'CO', 'NO2', 'SO2', 'O3'];
+const parameters = ['PM 2.5', 'PM 10', 'CO', 'NO2', 'SO2', 'O3'];
 
-const request = baseRequest.defaults({ timeout: REQUEST_TIMEOUT });
+// Get current date and time for query parameters
+function getNowDateFields () {
+  const dateNow = moment().tz('America/Guayaquil');
+  const dateFields = {
+    hour: dateNow.format('HH'),
+    dom: dateNow.format('DD'),
+    month: dateNow.format('MM'),
+    year: dateNow.format('YYYY')
+  };
+  return dateFields;
+}
 
-const renameParameter = function (parameter) {
-  switch (parameter) {
-    case 'NO2':
-      return 'no2';
-    case 'SO2':
-    case 'Sulfur Dioxide':
-      return 'so2';
-    case 'CO':
-      return 'co';
-    case 'PM2.5':
-    case 'PM 2.5':
-      return 'pm25';
-    case 'O3':
-      return 'o3';
-    case 'PM10':
-    case 'PM 10':
-      return 'pm10';
-    default:
-      return parameter;
+// Fix local date to adhere to format
+function sanitizeDate (date) {
+  var utc = moment.tz(date['utc'], 'America/Guayaquil');
+  return {
+    utc: date['utc'],
+    local: utc.format()
+  };
+}
+
+export async function fetchData (source, cb) {
+  try {
+    let queryParams = getNowDateFields();
+    // Get all the data for the day
+    // (only way to get the most recent data as it defaults to hour 0
+    // no matter what hour query parameter you send)
+    queryParams.itvl = '24 hours';
+    // Create promises with post requests and parsing for all parameters
+    const allParams = parameters.map(p =>
+      promiseRequest(source.url + '/dataset', { qs: { ...queryParams, ...{ magnitude: p } } })
+        // in case a request fails, handle gracefully
+        .catch(error => { log.warn(error || 'Unable to load data for parameter: ', p); return null; })
+        .then(data => formatData(data)));
+
+    const allData = await Promise.all(allParams);
+    const measurements = _.flatten((allData.filter(d => (d))));
+
+    cb(null, { name: 'unused', measurements });
+  } catch (e) {
+    cb(e);
   }
-};
+}
 
-const getNowDateFields = function () {
-  let dateObj = new Date();
-  let output = {};
-  output['month'] = dateObj.getUTCMonth() + 1;
-  output['day'] = dateObj.getUTCDate();
-  output['year'] = dateObj.getUTCFullYear();
-  return output;
-};
-const nowDateObject = getNowDateFields();
-
-var allMeasurements = [];
-
-exports.fetchData = function (source, cb) {
-  function sanitizeDate (date) {
-    try {
-      var utc = moment.tz(date['utc'], 'America/Guayaquil');
-      var local = moment.tz(date['local'], 'America/Guayaquil');
-      return {
-        utc: utc.toDate(),
-        local: local.format()
-      };
-    } catch (error) {
-      return cb({ message: 'Failed to parse date.' });
-    }
-  }
-
-  function formatData (_, data) {
-    let dataObject = JSON.parse(data);
-    dataObject.forEach((element) => {
-      let m = {
-        location: element['location'],
-        value: element['value'],
-        unit: element['unit'],
-        parameter: renameParameter(element['parameter']),
-        averagingPeriod: element['averagingPeriod'],
-        date: sanitizeDate(element['date']),
-        coordinates: element['coordinates'],
-        attribution: element['attribution'],
-        city: element['city'],
-        country: element['country'],
-        sourceType: element['sourceType'],
-        sourceName: element['sourceName'],
-        mobile: element['mobile'] === 'true'
-      };
-      allMeasurements.push(m);
-    });
-    return allMeasurements;
-  }
-
-  locations.forEach((station) => {
-    measurementTypes.forEach((measurement) => {
-      var propertiesObject = {
-        itvl: '1 hour',
-        year: nowDateObject['year'],
-        dom: nowDateObject['day'],
-        month: nowDateObject['month'],
-        magnitude: measurement,
-        location: station
-      };
-      request(
-        {
-          url: source.url,
-          headers: {
-            'User-Agent': 'OpenAQ',
-            Accept: '*/*',
-            Connection: 'keep-alive'
-          },
-          qs: propertiesObject
-        },
-        function (err, res, body) {
-          if (err || res.statusCode !== 200) {
-            return cb({ message: 'Failure to load data url.' });
-          }
-
-          try {
-            var data = formatData(source, body);
-
-            if (data === undefined) {
-              return cb({ message: 'Failure to parse data.' });
-            }
-          } catch (e) {
-            return cb({ message: 'Unknown adapter error.' });
-          }
-          return cb(null, {
-            name: 'unused',
-            measurements: allMeasurements
-          });
-        }
-      );
-    });
+// Convert data to standard format
+function formatData (data) {
+  if (!data) return null;
+  let dataObject = JSON.parse(data);
+  const paramMeasurements = dataObject.map(element => {
+    let m = {
+      location: element['location'],
+      value: Number(element['value']),
+      unit: element['unit'],
+      parameter: element['parameter'].replace(' ', ''),
+      averagingPeriod: element['averagingPeriod'],
+      date: sanitizeDate(element['date']),
+      coordinates: element['coordinates'],
+      attribution: element['attribution'],
+      city: element['city']
+    };
+    if (m.parameter === 'SulfurDioxide') m.parameter = 'so2';
+    return unifyParameters(m);
   });
-};
+  return paramMeasurements;
+}
