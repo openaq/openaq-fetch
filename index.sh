236bf094d2ffda6e0da8f076ad1ca038d25cb9b3
Rub21 <ruben@developmentseed.org>
2021-08-24 13:40:23 -0500

    Set prudent range time for executing fetch process

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@
 # This script intends to run the main fetch data process and other processes sequentially.
 # The script also has a timeout to kill the process in case it gets stuck.
 echo "Start main fetch process"
-timeout 5m npm start
+timeout 10m npm start
 
 echo "Start ccmaq - indian adapter"
-timeout 20m node index.js --source="caaqm"
+timeout 10m node index.js --source="caaqm"
