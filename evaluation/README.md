347ef0e9642040bc94902b34f582f378705109d9
Rub21 <ruben@developmentseed.org>
2021-11-16 09:15:16 -0500

    Update scripts to check last updates

---
+++ evaluation/README.md
@@ -0,0 +1,4 @@
+# Adapters evaluations
+
+## Last updates evaluations
+
--- evaluation/last_updates.py
+++ evaluation/last_updates.py
@@ -8,8 +8,21 @@ import requests
 from joblib import Parallel, delayed
 from tqdm import tqdm
 
+API_URL = "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?location={id}"
+SOURCE_FOLDER = "./../sources"
+ADPTERS_IDS_FILE = "data/adapters_id.csv"
+ADPTERS_LOCATION_LAST_UPDATES = "data/adapters_locations_last_updates.csv"
+
 
 def load_adapters(sources):
+    """Load data from json files in ../source folder
+
+    Args:
+        sources (str): Path to the source json files
+
+    Returns:
+        [dict]: Dictionary of all adapters dat
+    """
     adapters = []
     for jsonFile in glob.glob(f"{sources}/*.json"):
         with open(jsonFile) as json_file:
@@ -20,7 +33,16 @@ def load_adapters(sources):
 
 
 def fetch_data(adapter, sensor_nodes_id):
-    url = f"https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?location={sensor_nodes_id}"
+    """Fetch data from API
+
+    Args:
+        adapter (dict): Adapter data
+        sensor_nodes_id (int): Adapter id in the API
+
+    Returns:
+        [dict]: Returns datapter data + last updates data
+    """
+    url = API_URL.format(id=sensor_nodes_id)
     adapter_copy = adapter.copy()
     try:
         r = requests.get(url, timeout=20)
@@ -40,13 +62,16 @@ def fetch_data(adapter, sensor_nodes_id):
 
 
 def get_location_updates(adapter, df):
+    """Parrallel function to make request to the API
+
+    Args:
+        adapter (dict): Adapter data
+        df ([df]): Dataframe of adapters id
+
+    Returns:
+        [list]: List of dictinary of adapters with the last update
+    """
     df_adapter = df.loc[df["source_name"] == adapter["name"]]
-    # Serial
-    # results = []
-    # for _, row in df_adapter.iterrows():
-    #     adapter = fetch_data(adapter, row['sensor_nodes_id'])
-    #     results.append(adapter)
-    # Parallel
     adapter_name = adapter["name"]
     results = Parallel(n_jobs=-1)(
         delayed(fetch_data)(adapter, row["sensor_nodes_id"])
@@ -60,10 +85,9 @@ def get_location_updates(adapter, df):
 
 
 def main():
-    source_folder = "./../sources"
-    adpters_ids_file = "adapters_id.csv"
-    df = pd.read_csv(adpters_ids_file)
-    adapters = load_adapters(source_folder)
+    df = pd.read_csv(ADPTERS_IDS_FILE)
+    adapters = load_adapters(SOURCE_FOLDER)
+    # pd.DataFrame.from_dict(adapters).to_csv("adapters.csv", columns=["name"], index=False)
     keys = [
         "adapter",
         "name",
@@ -75,13 +99,13 @@ def main():
         "last_update",
     ]
 
-    with open("adapters_locations_last_updates.csv", "w") as f:
-        f.writelines(f'{",".join(keys)}\n')
+    with open(ADPTERS_LOCATION_LAST_UPDATES, "w") as f:
+        f.writelines(f'{"|".join(keys)}\n')
         for adapter in adapters:
             adapter_locations_lu = get_location_updates(adapter, df)
             for adapter_lu in adapter_locations_lu:
                 adapter_simple = dict((k, adapter_lu[k]) for k in adapter_lu.keys() if k in keys)
-                vals = ",".join(map(str, adapter_simple.values()))
+                vals = "|".join(map(str, adapter_simple.values()))
                 f.writelines(f"{vals}\n")
 
 
