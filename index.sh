1a20b129a782d965ec2dd45b3c8a1edd9be01ff0
Rub21 <ruben@developmentseed.org>
2021-11-19 09:04:36 -0500

    Update comment for index.sh

--- index.sh
+++ index.sh
@@ -3,9 +3,6 @@ set -ex
 # # This script intends to run the main fetch data process with timeout to kill the process in case it gets stuck.
 time timeout 10m npm start
 
-# The follow adapters are currently disabled, in sources/ , 
-# but actually we are requesting data. We disable because 
+# The follow adapters are currently disabled, in "sources/", but actually we are requesting data. We disable because 
 # those adapter request huge amount of data, that sometimes made failing the fetch process
 time timeout 5m node index.js --source="ARPALAZIO"
-
- 
