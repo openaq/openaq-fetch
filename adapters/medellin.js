e7f01a61dc939ed288f7a945bb1bea4b1b5a47c4
Rub21 <ruben@developmentseed.org>
2021-11-26 17:09:39 -0500

    Update Medellin adapter

--- adapters/medellin.js
+++ adapters/medellin.js
@@ -8,58 +8,89 @@
 
 import { default as moment } from 'moment-timezone';
 import { REQUEST_TIMEOUT } from '../lib/constants';
-import { default as baseRequest } from 'request';
-import { convertUnits } from '../lib/utils';
-import JSONStream from 'JSONStream';
-import { DataStream, MultiStream, StringStream } from 'scramjet';
-import { pick } from 'lodash';
-
-const request = baseRequest.defaults({timeout: REQUEST_TIMEOUT});
+import { promiseRequest } from '../lib/utils';
+import log from '../lib/logger';
+import https from 'https';
+import _ from 'lodash';
 
 exports.name = 'medellin';
+
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
+
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    'accept-language': 'en-US,en',
+    'content-type': 'application/x-www-form-urlencoded',
+    accept: 'application/json'
+  },
+  form: false,
+  timeout: REQUEST_TIMEOUT
+};
 
 /**
  * Fetches the data for a given source and returns an appropriate object
  * @param {object} source A valid source object
  * @param {function} cb A callback of the form cb(err, data)
  */
-exports.fetchStream = function (source) {
-  const pollutants = ['co', 'no2', 'ozono', 'pm10', 'pm25', 'so2'];
-  const streams = pollutants.map((pollutant) => {
-    const url = `${source.url}EntregaData1/Datos_SIATA_Aire_AQ_${pollutant}_Last.json`;
-    return new StringStream()
-      .use((stream) => {
-        const resp = request.get({ url }).on('response', ({ statusCode }) => {
-          +statusCode !== 200 ? stream.end() : resp.pipe(stream);
-        });
-        resp.on('error', function () {
-          resp.emit('close');
-          stream.end();
-        });
-        return stream;
-      })
-      .pipe(JSONStream.parse('measurements.*'))
-      .pipe(new DataStream())
-      .use((stream) => {
-        stream.name = 'unused';
-        return stream;
-      })
-      .map(extractMeasurements)
-      .map(convertUnits);
-  });
-  return new MultiStream(streams).mux();
-};
 
-const extractMeasurements = features => {
+export async function fetchData (source, cb) {
+  try {
+    const pollutants = ['co', 'no2', 'ozono', 'pm10', 'pm25', 'so2'];
+    // Create promises with post requests and parsing for all parameters
+    const allParams = pollutants.map((p) => {
+      const url = `${source.url}EntregaData1/Datos_SIATA_Aire_AQ_${p}_Last.json`;
+      const options = Object.assign(requestOptions, {
+        url: source.url,
+        body: Buffer.from('{"region":"landing_dashboard"}').toString('base64'),
+        agent
+      });
+      return promiseRequest(url, options)
+        .catch((error) => {
+          log.warn(error || `Unable to load data for parameter: ${p} for adapter ${source.name}`);
+          return null;
+        })
+        .then((data) => JSON.parse(data));
+    });
+
+    const allData = await Promise.all(allParams);
+
+    const items = _.flatten(
+      allData.map((a) => {
+        return a.measurements;
+      })
+    );
+    const measurements = items
+      .map((item) => extractMeasurements(item))
+      .filter((i) => i);
+    cb(null, { name: 'unused', measurements });
+  } catch (e) {
+    cb(e);
+  }
+}
+
+const extractMeasurements = (features) => {
   // Pick just the items we want
-  let o = pick(features, ['city', 'attribution', 'value', 'location', 'date', 'averagingPeriod', 'coordinates', 'parameter', 'unit', 'mobile']);
+  let o = _.pick(features, [
+    'city',
+    'attribution',
+    'value',
+    'location',
+    'date',
+    'averagingPeriod',
+    'coordinates',
+    'parameter',
+    'unit',
+    'mobile'
+  ]);
 
   // A few minor changes to match our format
   o.averagingPeriod.unit = o.averagingPeriod.units;
   delete o.averagingPeriod.units;
   o.attribution = [o.attribution];
-  o.parameter = (o.parameter === 'pm10³') ? 'pm10' : o.parameter;
-
+  o.parameter = o.parameter === 'pm10³' ? 'pm10' : o.parameter;
   // And generate the date
   const date = moment.tz(features.date.utc, 'America/Bogota');
   o.date = {
@@ -67,5 +98,8 @@ const extractMeasurements = features => {
     local: date.format()
   };
 
+  if (o.value <= -9999) {
+    return null;
+  }
   return o;
 };
