f5af6fb64dde197fd14fa9d6ac07264818198469
Christian Parker <cparker@talloaks-environmental.com>
2023-03-15 14:57:48 -0700

    Modified the script to update providers and adapters

--- data_scripts/update-db-sources.js
+++ data_scripts/update-db-sources.js
@@ -7,17 +7,19 @@ const db = knex(config);
 console.log(`There are currently ${sources.length} sources locally`);
 
 // check to see what keys are in the sources
-console.log(
-  sources.map(data => Object.keys(data))
-    .flat()
-    .reduce((acc, curr) => {
-      acc[curr] = -~acc[curr];
-      return acc;
-    }, {})
-);
+// console.log(
+//   sources.map(data => Object.keys(data))
+//     .flat()
+//     .reduce((acc, curr) => {
+//       acc[curr] = -~acc[curr];
+//       return acc;
+//     }, {})
+// );
 
 // reshape to match what is in the database
-const sources_db = sources.map(data => {
+const sources_db = sources
+      .filter(d => d.name!='test_adapter')
+      .map(data => {
   let interval;
   switch (data.resolution) {
   case '10 min':
@@ -35,16 +37,56 @@ const sources_db = sources.map(data => {
     description: data.description,
     source_name: data.name,
     export_prefix: data.adapter,
+    adapter: data.adapter,
+    active: data.active,
     metadata: data,
   };
 });
 
+
 (async () => {
-  let upserts = await db('providers')
+
+  if(!(await db.schema.hasTable('providers_temp'))) {
+    await db.schema.createTable('providers_temp', table => {
+      table.string('label');
+      table.string('description');
+      table.string('source_name');
+      table.string('export_prefix');
+      table.string('adapter');
+      table.boolean('active');
+      table.json('metadata');
+    });
+  };
+
+  let upserts = await db('providers_temp')
       .insert(sources_db)
-      .onConflict("source_name")
-      .merge(['metadata'])
       .returning("*");
+
+  await db.raw(
+    `INSERT INTO adapters (name, description, handler)
+     SELECT adapter
+     , 'Automatically added from fetcher data'
+     , 'openaq-fetch'
+     FROM providers_temp
+     ON CONFLICT DO NOTHING`
+  );
+
+  await db.raw(
+    `UPDATE providers
+     SET metadata = p.metadata
+     , is_active = p.active
+     FROM providers_temp p
+     WHERE providers.source_name = p.source_name`
+  );
+
+  await db.raw(
+    `UPDATE providers
+     SET adapters_id = a.adapters_id
+     FROM adapters a
+     WHERE providers.metadata->>'adapter' = a.name`
+  );
+
+  await db.schema.dropTable('providers_temp');
 
   console.log(`Upserted ${upserts.length} providers to the db`);
 
