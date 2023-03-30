e4fadd2564086913651b7d4a69e21ebdf9be71d1
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-03-30 10:50:25 -0600

    corrected discrepancy with utc/local time

--- src/adapters/hungary.js
+++ src/adapters/hungary.js
@@ -8,7 +8,7 @@ import { DateTime } from 'luxon';
 import fetch from 'node-fetch'
 
 // Get the current time in Hungary
-let dt = DateTime.local().setZone('Europe/Budapest');
+let dt = DateTime.utc();
 const { year, month, day } = dt.toObject({ year: 'numeric', month: 'numeric', day: 'numeric' });
 
 export const name  = 'hungary'
@@ -26,6 +26,7 @@ export async function fetchData (source, cb) {
       if (station.hasOwnProperty('stationId')) {
         const stationId = station.stationId;
         const url = `${source.url}${stationId}`
+        // console.log(url)
         return fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -39,8 +40,8 @@ export async function fetchData (source, cb) {
             // Add fetched data to station object
             station.station = data.data.stationName;
             station.month = data.data.month;
-            station.utc = date.toUTC().toISO({suppressMilliseconds: true}),
-            station.local = date.toISO({suppressMilliseconds: true});
+            station.utc = dt.toFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            station.local = dt.setZone("Europe/Budapest").toFormat("yyyy-MM-dd'T'HH:mm:ssZZ");
             station.measurements = data.data.lastHourValues;
             return station;
           })
@@ -107,7 +108,6 @@ async function formatData(input) {
   measurements = removeUnwantedParameters(measurements);
   measurements = filterMeasurements(measurements);
   measurements = getLatestMeasurements(measurements);
-  
   return { name: 'unused', measurements: measurements }
 };
 
