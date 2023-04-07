d793a5fd63b245515952862ca5bb10f6335abbcf
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-04-07 15:21:43 -0600

    update

--- scripts/process_depcheck_output.sh
+++ scripts/process_depcheck_output.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
-DEPCHECK_RESULT=$1
+
+DEPCHECK_RESULT="$1"
+
 MISSING=$(echo "$DEPCHECK_RESULT" | jq '.missing')
 MISSING_LENGTH=$(echo "$MISSING" | jq 'length')
+
 DEPENDENCIES=$(echo "$DEPCHECK_RESULT" | jq '.dependencies')
 DEPENDENCIES_LENGTH=$(echo "$DEPENDENCIES" | jq 'length')
+
 DEV_DEPENDENCIES=$(echo "$DEPCHECK_RESULT" | jq '.devDependencies')
 DEV_DEPENDENCIES_LENGTH=$(echo "$DEV_DEPENDENCIES" | jq 'length')
+
 comment=""
-if [ "$MISSING_LENGTH" -gt 0 ]; then
-    comment+="The following packages are missing:\n"
-    for key in $(echo "$MISSING" | jq 'keys[]'); do
-        package=$(echo "$key" | jq -r)
-        needed_by=$(echo "$MISSING" | jq -r ".[$package] | join(\", \")")
-        comment+="$package is needed by $needed_by\n"
-        done
-    comment+="\n"
+
+if [ $MISSING_LENGTH -gt 0 ]; then
+  comment+="The following packages are missing:\n"
+  for key in $(echo "$MISSING" | jq 'keys[]'); do
+    package=$(echo "$key" | jq -r)
+    needed_by=$(echo "$MISSING" | jq -r ".[$key] | join(\", \")")
+    comment+="$package is needed by $needed_by\n"
+  done
+  comment+="\n"
 fi
-if [ "$DEPENDENCIES_LENGTH" -gt 0 ] || [ "$DEV_DEPENDENCIES_LENGTH" -gt 0 ]; then
-    total_unused=$((DEPENDENCIES_LENGTH + DEV_DEPENDENCIES_LENGTH))
-    comment+="There are $total_unused unused packages:\n"
-if [ "$DEPENDENCIES_LENGTH" -gt 0 ]; then
+
+if [ $DEPENDENCIES_LENGTH -gt 0 ] || [ $DEV_DEPENDENCIES_LENGTH -gt 0 ]; then
+  total_unused=$((DEPENDENCIES_LENGTH + DEV_DEPENDENCIES_LENGTH))
+  comment+="There are $total_unused unused packages:\n"
+
+  if [ $DEPENDENCIES_LENGTH -gt 0 ]; then
     comment+="Unused dependencies:\n"
     comment+=$(echo "$DEPENDENCIES" | jq -r 'join(", ")')
     comment+="\n"
-fi
-if [ "$DEV_DEPENDENCIES_LENGTH" -gt 0 ]; then
+  fi
+
+  if [ $DEV_DEPENDENCIES_LENGTH -gt 0 ]; then
     comment+="Unused devDependencies:\n"
     comment+=$(echo "$DEV_DEPENDENCIES" | jq -r 'join(", ")')
+  fi
 fi
-fi
-echo -n "$comment" | base64
+
+echo -n "$comment"
