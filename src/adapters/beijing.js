950bd5b94a0d768d374b61d1515962256f1bcd24
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-04-12 16:33:05 -0600

    updated deprecated libraries for beijing.js

--- src/adapters/beijing.js
+++ src/adapters/beijing.js
@@ -5,39 +5,43 @@
 'use strict';
 
 import { REQUEST_TIMEOUT } from '../lib/constants.js';
-import { default as baseRequest } from 'request';
+import got from 'got';
 import cloneDeep from 'lodash/cloneDeep.js';
-import { default as moment } from 'moment-timezone';
+import { DateTime } from 'luxon';
 import cheerio from 'cheerio';
-const request = baseRequest.defaults({timeout: REQUEST_TIMEOUT});
 
 export const name = 'beijing';
 
 /**
  * Fetches the data for a given source and returns an appropriate object
  * @param {object} source A valid source object
- * @param {function} cb A callback of the form cb(err, data)
+ * @return {Promise<object>} A Promise that resolves to the fetched data
  */
+
 export const fetchData = function (source, cb) {
-  request(source.url, function (err, res, body) {
-    if (err || res.statusCode !== 200) {
-      return cb({message: 'Failure to load data url.'});
-    }
-
-    // Wrap everything in a try/catch in case something goes wrong
-    try {
-      // Format the data
-      var data = formatData(body, source);
-
-      // Make sure the data is valid
-      if (data === undefined) {
-        return cb({message: 'Failure to parse data.'});
+  got(source.url, { timeout: { request: REQUEST_TIMEOUT } })
+    .then((response) => {
+      if (response.statusCode !== 200) {
+        return cb({ message: 'Failure to load data url.' });
       }
-      cb(null, data);
-    } catch (e) {
-      return cb({message: 'Unknown adapter error.'});
-    }
-  });
+
+      // Wrap everything in a try/catch in case something goes wrong
+      try {
+        // Format the data
+        let data = formatData(response.body, source);
+
+        // Make sure the data is valid
+        if (data === undefined) {
+          return cb({ message: 'Failure to parse data.' });
+        }
+        cb(null, data);
+      } catch (e) {
+        return cb({ message: 'Unknown adapter error.' });
+      }
+    })
+    .catch((error) => {
+      return cb({ message: 'Failure to load data url.', error });
+    });
 };
 
 /**
@@ -46,50 +50,76 @@ export const fetchData = function (source, cb) {
  * @param {object} source A valid source object
  * @return {object} Parsed and standardized data our system can use
  */
-var formatData = function (data, source) {
+const formatData = function (data, source) {
   /**
    * Given a date string, convert to system appropriate times.
    * @param {string} dateString Date in string format coming from source data
    * @return {object} An object containing both UTC and local times
    */
-  var getDate = function (dateString) {
-    var date = moment.tz(dateString, 'MM/DD/YYYY HH:mm:ss A', 'Asia/Shanghai');
-    return {utc: date.toDate(), local: date.format()};
+  const getDate = function (dateString) {
+    // Validate input
+    if (typeof dateString !== 'string' || dateString.trim() === '') {
+      throw new Error('Invalid date string');
+    }
+
+    try {
+      // Create DateTime object with timezone using the fromFormat() method
+      const date = DateTime.fromFormat(
+        dateString,
+        'M/d/yyyy h:mm:ss a',
+        {
+          zone: 'Asia/Shanghai',
+        }
+      );
+
+      // Return UTC and local ISO strings
+      return {
+        utc: date.toISO({
+          suppressMilliseconds: true,
+          includeOffset: true,
+        }),
+        local: date.toISO({ suppressMilliseconds: true }),
+      };
+    } catch (error) {
+      console.error('Error parsing date:', error);
+      throw new Error('Error parsing date');
+    }
   };
 
   // Load all the XML
-  var $ = cheerio.load(data, {xmlMode: true});
+  let $ = cheerio.load(data, { xmlMode: true });
 
   // Create measurements array
-  var measurements = [];
+  let measurements = [];
 
   // Build up the base object
-  var baseObj = {
+  let baseObj = {
     location: source.name,
     parameter: 'pm25',
     unit: 'µg/m³',
-    averagingPeriod: {'value': 1, 'unit': 'hours'},
-    attribution: [{
-      name: 'StateAir.net',
-      url: source.sourceURL
-    }],
-    coordinates: getCoordinates(source.name)
+    averagingPeriod: { value: 1, unit: 'hours' },
+    attribution: [
+      {
+        name: 'StateAir.net',
+        url: source.sourceURL,
+      },
+    ],
+    coordinates: getCoordinates(source.name),
   };
 
   // Loop over each item and save the object
   $('item').each(function (i, elem) {
     // Clone base object
-    var obj = cloneDeep(baseObj);
+    let obj = cloneDeep(baseObj);
 
     obj.value = Number($(elem).children('Conc').text());
     obj.date = getDate($(elem).children('ReadingDateTime').text());
-
     measurements.push(obj);
   });
 
   return {
     name: 'unused',
-    measurements: measurements
+    measurements: measurements,
   };
 };
 
@@ -98,27 +128,27 @@ export const getCoordinates = function (location) {
     case 'Beijing US Embassy':
       return {
         latitude: 39.95,
-        longitude: 116.47
+        longitude: 116.47,
       };
     case 'Chengdu':
       return {
         latitude: 30.63,
-        longitude: 104.07
+        longitude: 104.07,
       };
     case 'Guangzhou':
       return {
         latitude: 23.12,
-        longitude: 113.32
+        longitude: 113.32,
       };
     case 'Shanghai':
       return {
         latitude: 31.21,
-        longitude: 121.44
+        longitude: 121.44,
       };
     case 'Shenyang':
       return {
         latitude: 41.78,
-        longitude: 123.42
+        longitude: 123.42,
       };
   }
 };
