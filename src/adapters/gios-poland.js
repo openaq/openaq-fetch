8462e9c2c486f16aaaadad03bf4af40b14ed1b01
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-04-18 10:36:48 -0600

    replaced deprecated librairies for gios-poland.js

--- src/adapters/gios-poland.js
+++ src/adapters/gios-poland.js
@@ -1,31 +1,25 @@
-import { REQUEST_TIMEOUT } from '../lib/constants.js';
-import request from 'request';
-import rp from 'request-promise-native';
-import { parse as JSONStream } from 'JSONStream';
+'use strict';
+
 import sj from 'scramjet';
-const { DataStream } = sj;
 import { FetchError, DATA_URL_ERROR } from '../lib/errors.js';
 import log from '../lib/logger.js';
 import { acceptableParameters } from '../lib/utils.js';
 import { DateTime } from 'luxon';
 import got from 'got';
 
-const gotExtended = got.extend({
-	retry: { limit: 3 },
-	timeout: { request: REQUEST_TIMEOUT },
-	});
-  
+const { DataStream } = sj;
+
 const resolveParameter = (param) =>
   param.toLowerCase().replace('c6h6', 'bc').replace('.', '');
-// const makeDate = (date) => moment.tz(date, 'Europe/Warsaw');
+
 const makeDate = (date) =>
   DateTime.fromFormat(date, 'yyyy-MM-dd HH:mm:ss', {
-    zone: 'Europe/Warsaw',
+    zone: 'Europe/Warsaw'
   });
 
 export const name = 'gios-poland';
 
-export function fetchStream(source) {
+export async function fetchStream (source) {
   const {
     url,
     country,
@@ -37,108 +31,108 @@ export function fetchStream(source) {
   } = source;
 
   const stationUrl = `${url}/station/findAll`;
-  const requestObject = request.get(stationUrl);
-  return DataStream.pipeline(requestObject, JSONStream('*'))
-    .catch((e) => {
-      requestObject.abort();
-      e.stream.end();
-      throw e;
-    })
-    .map(
-      ({
-        id,
-        stationName,
-        gegrLat,
-        gegrLon,
-        city: { name: city },
-      }) => ({
-        stationId: id,
-        base: {
-          location: stationName,
-          coordinates: {
-            latitude: +gegrLat,
-            longitude: +gegrLon,
+
+  try {
+    const stations = JSON.parse(await got(stationUrl).text());
+    return DataStream.from(stations)
+      .map(
+        ({
+          id,
+          stationName,
+          gegrLat,
+          gegrLon,
+          city: { name: city },
+        }) => ({
+          stationId: id,
+          base: {
+            location: stationName,
+            coordinates: {
+              latitude: +gegrLat,
+              longitude: +gegrLon,
+            },
+            city,
+            country,
           },
-          city,
-          country,
-        },
-        attribution,
-        averagingPeriod,
-        sourceType,
-        mobile,
+          attribution,
+          averagingPeriod,
+          sourceType,
+          mobile,
+        })
+      )
+      .flatMap(async ({ stationId, base }) => {
+        const _url = `${url}/station/sensors/${stationId}`;
+        log.debug(`Getting sensors for station ${stationId}`);
+        try {
+          const sensors = JSON.parse(await got(_url).text());
+
+          return sensors
+            .map(({ id: sensorId, param: { paramCode } }) => ({
+              sensorId,
+              parameter: resolveParameter(paramCode),
+              base,
+            }))
+            .filter(({ parameter }) =>
+              acceptableParameters.includes(parameter)
+            );
+        } catch (e) {
+          log.debug(`Error while fetching data from ${_url}`);
+          throw new FetchError(
+            DATA_URL_ERROR,
+            source,
+            e,
+            `Error fetching or parsing data for station`
+          );
+        }
       })
-    )
-    .flatMap(async ({ stationId, base }) => {
-      const _url = `${url}/station/sensors/${stationId}`;
-      log.debug(`Getting sensors for station ${stationId}`);
-      try {
-        const sensors = JSON.parse(await rp(_url));
+      .flatMap(async ({ sensorId, parameter, base }) => {
+        const _url = `${url}/data/getData/${sensorId}`;
+        try {
+          const data = JSON.parse(await got(_url).text());
+          const unit = 'µg/m³';
 
-        return sensors
-          .map(({ id: sensorId, param: { paramCode } }) => ({
-            sensorId,
-            parameter: resolveParameter(paramCode),
-            base,
-          }))
-          .filter(({ parameter }) =>
-            acceptableParameters.includes(parameter)
-          );
-      } catch (e) {
-        log.debug(`Error while fetching data from ${_url}`);
-        throw new FetchError(
-          DATA_URL_ERROR,
-          source,
-          e,
-          `Error fetching or parsing data for station`
-        );
-      }
-    })
-    .flatMap(async ({ sensorId, parameter, base }) => {
-      const _url = `${url}/data/getData/${sensorId}`;
-      try {
-        const data = JSON.parse(await rp(_url));
-        const unit = 'µg/m³';
+          const values = Array.from(data.values);
+          const lastIndex = values.findIndex((item) => {
+            item.tzDate = makeDate(item.date).toISO({
+              suppressMilliseconds: true,
+            });
 
-        const values = Array.from(data.values);
-        const lastIndex = values.findIndex((item) => {
-          item.tzDate = makeDate(item.date).toISO({
-            suppressMilliseconds: true,
+            const currentDate = DateTime.fromISO(item.tzDate, {
+              zone: 'utc',
+            });
+            const thresholdDate = DateTime.local()
+              .toUTC()
+              .minus({ hours: hoursToFetch });
+
+            if (currentDate < thresholdDate) {
+              return true;
+            }
           });
 
-          const currentDate = DateTime.fromISO(item.tzDate, {
-            zone: 'utc',
-          });
-          const thresholdDate = DateTime.local()
-            .toUTC()
-            .minus({ hours: hoursToFetch });
-
-          if (currentDate < thresholdDate) {
-            return true;
-          }
-        });
-
-        return values
-          .slice(0, lastIndex)
-          .filter(({ value }) => value !== null)
-          .map(({ tzDate, value }) =>
-            Object.assign(
-              {
-                parameter,
-                unit,
-                value: +value,
-                date: { local: tzDate },
-              },
-              base
-            )
+          return values
+            .slice(0, lastIndex)
+            .filter(({ value }) => value !== null)
+            .map(({ tzDate, value }) =>
+              Object.assign(
+                {
+                  parameter,
+                  unit,
+                  value: +value,
+                  date: { local: tzDate },
+                },
+                base
+              )
+            );
+        } catch (e) {
+          log.debug(`Error while fetching data from ${_url}`);
+          throw new FetchError(
+            DATA_URL_ERROR,
+            source,
+            e,
+            `code: ${e.statusCode}`
           );
-      } catch (e) {
-        log.debug(`Error while fetching data from ${_url}`);
-        throw new FetchError(
-          DATA_URL_ERROR,
-          source,
-          e,
-          `code: ${e.statusCode}`
-        );
-      }
-    });
+        }
+      });
+  } catch {
+    console.log('error resolving url');
+  }
 }
