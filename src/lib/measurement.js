9e5e4f4724d4f6a735a8cd73a8d74295fd81874e
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-05-02 16:17:40 -0600

    works?

--- src/lib/measurement.js
+++ src/lib/measurement.js
@@ -11,22 +11,18 @@ import {
   MeasurementValidationError,
   handleMeasurementErrors,
   AdapterError,
-  forwardErrors
+  forwardErrors,
 } from './errors.js';
 import { getAdapterForSource } from './adapters.js';
-import {
-  readFileSync,
-  existsSync,
-  readdirSync
-} from 'fs';
+import { readFileSync, existsSync, readdirSync } from 'fs';
 
 import {
   ignore,
   unifyMeasurementUnits,
   removeUnwantedParameters,
-  unifyParameters
+  unifyParameters,
 } from './utils.js';
-import moment from 'moment';
+import { DateTime } from 'luxon';
 
 /**
  * @typedef MeasurementObject
@@ -39,20 +35,32 @@ import moment from 'moment';
  * @prop {Promise} whenDone promise resolved on measurements fetch completion
  */
 
-const measurementSchema = JSON.parse(readFileSync(`./lib/measurement-schema.json`, 'utf8'));
+const measurementSchema = JSON.parse(
+  readFileSync(`./lib/measurement-schema.json`, 'utf8')
+);
 
-async function getStreamFromAdapter (adapter, source) {
-  log.debug(`Getting stream for "${source.name}" from "${adapter.name}"`);
+async function getStreamFromAdapter(adapter, source) {
+  log.debug(
+    `Getting stream for "${source.name}" from "${adapter.name}"`
+  );
 
   if (!adapter.fetchStream) {
-    log.debug(`Getting data for "${source && source.name}" from adapter "${adapter.name}"`);
-    const data = await (promisify(adapter.fetchData)(source));
+    log.debug(
+      `Getting data for "${source && source.name}" from adapter "${
+        adapter.name
+      }"`
+    );
+    const data = await promisify(adapter.fetchData)(source);
     const out = DataStream.from(data.measurements);
     out.name = data.name;
     return out;
   }
 
-  log.debug(`Fetching stream for "${source && source.name}" from adapter "${adapter.name}"`);
+  log.debug(
+    `Fetching stream for "${source && source.name}" from adapter "${
+      adapter.name
+    }"`
+  );
   const out = DataStream.from(adapter.fetchStream, source);
   out.name = out.name || source.adapter;
   return out;
@@ -63,125 +71,136 @@ async function getStreamFromAdapter (adapter, source) {
  *
  * @returns {MeasurementObject}
  */
-function createFetchObject (input, source, failures, dryRun) {
+function createFetchObject(input, source, failures, dryRun) {
   let fetchEnded = NaN;
   const counts = {
     total: 0,
     duplicates: 0,
-    inserted: 0
+    inserted: 0,
   };
 
-  const stream = input
-    .do(() => counts.total++)
-  ;
-
+  const stream = input.do(() => counts.total++);
   const whenDone = stream
     .whenEnd()
-    .then(() => { fetchEnded = Date.now(); })
+    .then(() => {
+      fetchEnded = Date.now();
+    })
     .catch(ignore);
 
   return {
     fetchStarted: Date.now(),
-    get fetchEnded () {
+    get fetchEnded() {
       return fetchEnded;
     },
-    get duration () {
+    get duration() {
       return ((fetchEnded || Date.now()) - this.fetchStarted) / 1000;
     },
-    get failures () {
+    get failures() {
       return fetchEnded ? failures : null;
     },
-    get count () {
+    get count() {
       return fetchEnded && (!dryRun ? counts.inserted : counts.total);
     },
-    get message () {
-      return `${dryRun ? '[Dry Run] ' : ''}New measurements found for ${source.name}: ${this.count}`;
+    get message() {
+      return `${
+        dryRun ? '[Dry Run] ' : ''
+      }New measurements found for ${source.name}: ${this.count}`;
     },
     dryRun,
     stream,
     source,
     counts,
     whenDone,
-    get resultsMessage () {
+    get resultsMessage() {
       return fetchEnded
         ? {
-          message: this.message,
-          failures: this.failures,
-          count: this.count,
-          duration: this.duration,
-          sourceName: this.source.name
-        }
+            message: this.message,
+            failures: this.failures,
+            count: this.count,
+            duration: this.duration,
+            sourceName: this.source.name,
+          }
         : null;
-    }
+    },
   };
 }
 
-function normalizeDate (measurement) {
+function normalizeDate(measurement) {
   if (measurement.date) {
     if (measurement.date instanceof Date) {
       measurement.date = {
-        local: moment(measurement.date).format()
+        local: DateTime.fromJSDate(measurement.date).toISO(),
       };
     }
 
     if (!measurement.date.utc && measurement.date.local) {
-      measurement.date.utc = +new Date(measurement.date.local);
+      measurement.date.utc = DateTime.fromISO(measurement.date.local).toUTC().toMillis();
+    }
+
+    if (typeof measurement.date.utc === 'string') {
+      measurement.date.utc = DateTime.fromISO(measurement.date.utc).toMillis();
+    } else if (measurement.date.utc instanceof Date) {
+      measurement.date.utc = measurement.date.utc.getTime();
     }
 
     if (measurement.date.utc) {
-      measurement.date.utc = new Date(measurement.date.utc).toISOString();
+      measurement.date.utc = DateTime.fromMillis(measurement.date.utc).toUTC().toISO();
     }
   }
 
   return measurement;
 }
 
-function checkLocation (measurement) {
-  if(measurement.country === ''
-     && measurement.coordinates
-     && measurement.coordinates.latitude
-     && measurement.coordinates.longitude) {
+function checkLocation(measurement) {
+  if (
+    measurement.country === '' &&
+    measurement.coordinates &&
+    measurement.coordinates.latitude &&
+    measurement.coordinates.longitude
+  ) {
     measurement.country = '99';
   }
   return measurement;
 }
 
-function fixMeasurements (stream, source) {
+function fixMeasurements(stream, source) {
   return stream
     .do(normalizeDate)
     .do(unifyMeasurementUnits)
     .do(unifyParameters)
     .do(checkLocation)
-    .map(({
-      date,
-      parameter,
-      value,
-      unit,
-      averagingPeriod,
-      location,
-      city,
-      country,
-      coordinates,
-      attribution,
-      sourceType,
-      sourceName,
-      mobile
-    }) => ({
-      date,
-      parameter,
-      value,
-      unit,
-      averagingPeriod,
-      location: location || source.location || source.name,
-      city: city || source.city,
-      country: country || source.country,
-      coordinates,
-      attribution,
-      sourceName: sourceName || source.name,
-      sourceType: sourceType || source.type || 'government',
-      mobile: typeof mobile === 'undefined' ? !!source.mobile : mobile
-    }))
-  ;
+    .map(
+      ({
+        date,
+        parameter,
+        value,
+        unit,
+        averagingPeriod,
+        location,
+        city,
+        country,
+        coordinates,
+        attribution,
+        sourceType,
+        sourceName,
+        mobile,
+      }) => ({
+        date,
+        parameter,
+        value,
+        unit,
+        averagingPeriod,
+        location: location || source.location || source.name,
+        city: city || source.city,
+        country: country || source.country,
+        coordinates,
+        attribution,
+        sourceName: sourceName || source.name,
+        sourceType: sourceType || source.type || 'government',
+        mobile:
+          typeof mobile === 'undefined' ? !!source.mobile : mobile,
+      })
+    );
 }
 
 /**
@@ -191,29 +210,31 @@ function fixMeasurements (stream, source) {
  * @return {DataStream<Measurement>} A stream pruned of invalid measurement objects, may be empty
  *                                   and a failures object of aggregated reasons for cause failures
  */
-function validateMeasurements (stream, source) {
-  const out = stream.map(
-    async measurement => {
-      let v = validate(measurement, measurementSchema);
-      if (v.errors.length === 0) {
-        return measurement;
-      } else {
-        throw new MeasurementValidationError(source, {measurement, errors: v.errors});
-      }
+function validateMeasurements(stream, source) {
+  const out = stream.map(async (measurement) => {
+    let v = validate(measurement, measurementSchema);
+    if (v.errors.length === 0) {
+      return measurement;
+    } else {
+      throw new MeasurementValidationError(source, {
+        measurement,
+        errors: v.errors,
+      });
     }
-  );
+  });
 
   return out;
 }
 
-export async function getCorrectedMeasurementsFromSource (source, env) {
+export async function getCorrectedMeasurementsFromSource(
+  source,
+  env
+) {
   if (source instanceof Error) throw source;
 
-  const [ret] = await (
-    DataStream.from([source])
-      .use(fetchCorrectedMeasurementsFromSourceStream, {strict: true})
-      .toArray()
-  );
+  const [ret] = await DataStream.from([source])
+    .use(fetchCorrectedMeasurementsFromSourceStream, { strict: true })
+    .toArray();
 
   return ret;
 }
@@ -225,59 +246,57 @@ export async function getCorrectedMeasurementsFromSource (source, env) {
  * @param {OpenAQEnv} env
  *
  */
-export function fetchCorrectedMeasurementsFromSourceStream (stream, env) {
+
+export function fetchCorrectedMeasurementsFromSourceStream(stream, env) {
   log.debug(`Fetching corrected measurements from a stream of sources`);
-  return stream.into(
-    async (out, source) => {
-      const failures = {};
-      const input = new DataStream();
-      let error = null;
+  return stream.into(async (out, source) => {
+    const failures = {};
+    const input = new DataStream();
+    let error = null;
 
-      const output = input
-        .use(fixMeasurements, source)
-        .use(validateMeasurements, source)
-        .use(removeUnwantedParameters)
-        .use(handleMeasurementErrors, failures, source)
-        .use(forwardErrors, stream, source, failures, env)
-      ;
+    const output = input
+      .use(fixMeasurements, source)
+      .use(validateMeasurements, source)
+      .use(removeUnwantedParameters)
+      .use(handleMeasurementErrors, failures, source)
+      .use(forwardErrors, stream, source, failures, env);
 
-
-      if(env.datetime) {
-        source.datetime = moment.utc(env.datetime, true);
-        if(!source.datetime.isValid()) {
-          throw new Error('Invalid date/time');
-        }
-        log.debug(`Using env datetime of ${env.datetime}`);
-      } else if(source.offset) {
-        source.datetime = moment.utc().subtract(source.offset, 'hours');
-        log.debug(`Using source offset of ${source.offset} hours to set datettime`);
-      } else if(env.offset) {
-        source.datetime = moment.utc().subtract(env.offset, 'hours');
-        source.offset = env.offset;
-        log.debug(`Using env offset of ${env.offset} hours to set datettime`);
-      } else {
-        log.debug(`No offset or datetime being used`);
+    if (env.datetime) {
+      source.datetime = DateTime.fromISO(env.datetime, { zone: 'utc' });
+      if (!source.datetime.isValid) {
+        throw new Error('Invalid date/time');
       }
+      log.debug(`Using env datetime of ${source.datetime}`);
+    } else if (source.offset) {
+      source.datetime = DateTime.utc().minus({ hours: source.offset });
+      log.debug(`Using source offset of ${source.offset} hours to set datetime`);
+    } else if (env.offset) {
+      source.datetime = DateTime.utc().minus({ hours: env.offset });
+      source.offset = env.offset;
+      log.debug(`Using env offset of ${env.offset} hours to set datetime`);
+    } else {
+      log.debug(`No offset or datetime being used`);
+    }
 
-      try {
-        log.debug(`Looking up adapter for source "${source && source.name}"`);
-        const adapter = await getAdapterForSource(source);
+    try {
+      log.debug(`Looking up adapter for source "${source && source.name}"`);
+      const adapter = await getAdapterForSource(source);
 
-        (await getStreamFromAdapter(adapter, source)).pipe(input);
+      (await getStreamFromAdapter(adapter, source)).pipe(input);
 
-        if (error) throw error;
-        else error = true;
-      } catch (cause) {
-        await (
-          input.raise(
-            cause instanceof AdapterError ? cause : new AdapterError(ADAPTER_ERROR, source, cause)
-          )
-        );
-        input.end();
-      }
+      if (error) throw error;
+      else error = true;
+    } catch (cause) {
+      await input.raise(
+        cause instanceof AdapterError
+          ? cause
+          : new AdapterError(ADAPTER_ERROR, source, cause)
+      );
+      input.end();
+    }
 
-      await out.whenWrote(createFetchObject(output, source, failures, env.dryrun));
-    },
-    new DataStream()
-  );
+    await out.whenWrote(
+      createFetchObject(output, source, failures, env.dryrun)
+    );
+  }, new DataStream());
 }
