b45205c64819b6c0c18341f2e784b754d102b6c4
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-05-22 17:15:01 -0600

    Basilicata, IT is live!

--- src/adapters/basilicata.js
+++ src/adapters/basilicata.js
@@ -1,14 +1,16 @@
+import { REQUEST_TIMEOUT } from '../lib/constants.js';
+import log from '../lib/logger.js';
+
 import puppeteer from 'puppeteer';
 import { DateTime } from 'luxon';
-import fetch from 'node-fetch';
 import got from 'got';
 
-const bearerAuth = await getBearerAuth().then((bearerAuth) => bearerAuth);
+const bearerAuth = await getBearerAuth().then(
+  (bearerAuth) => bearerAuth
+);
 
 const STATIONS_URL =
   'https://arpabaegis.arpab.it/Datascape/v3/locations?category=All&basin_org_id&basin_id&region_id&province_id&station_id&filter_central_id&filter_id&_=1677195752467';
-const MEASUREMENTS_URL =
-  'https://arpabaegis.arpab.it/Datascape/v3/elements?station_id=951100&longitude&latitude&category=1&ui_culture=en&field=ElementName&field=Time&field=Value&field=Decimals&field=MeasUnit&field=Trend&field=StateId&field=IsQueryable&filter_central_id&filter_id&_=1677195752563';
 
 const HEADERS = {
   authorization: bearerAuth,
@@ -28,54 +30,60 @@ const HEADERS = {
   'Referrer-Policy': 'strict-origin-when-cross-origin',
 };
 
-async function fetchData() {
+const getter = got.extend({
+  retry: { limit: 3 },
+  timeout: { request: REQUEST_TIMEOUT },
+  method: 'GET',
+  headers: HEADERS,
+});
+
+export const name = 'basilicata';
+
+export async function fetchData(source, cb) {
   let stations = await fetchStations();
 
-  // Map through each station object and fetch data for each station
-  let requests = stations.map((station) => {
+  let requests = stations.map(async (station) => {
     if (station.hasOwnProperty('i')) {
       const stationId = station.i;
-      const url = `https://arpabaegis.arpab.it/Datascape/v3/elements?station_id=${stationId}&longitude&latitude&category=1&ui_culture=en&field=ElementName&field=Time&field=Value&field=Decimals&field=MeasUnit&field=Trend&field=StateId&field=IsQueryable&filter_central_id&filter_id&_=1677195752563`;
-      const options = {
-        method: 'GET',
-        headers: HEADERS,
-        body: null,
-      };
-      return fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => {
-          // Add fetched data to station object
-          station.data = data;
-          return station;
-        })
-        .catch((error) => {
-          throw error;
-        });
+      const url = `${source.url}${stationId}&longitude&latitude&category=1&ui_culture=en&field=ElementName&field=Time&field=Value&field=Decimals&field=MeasUnit&field=Trend&field=StateId&field=IsQueryable&filter_central_id&filter_id&_=`;
+      try {
+        const response = await getter(url);
+        const data = JSON.parse(response.body);
+        // Add fetched data to station object
+        station.data = data;
+        return station;
+      } catch (error) {
+        log.error(
+          `Failed to fetch data for station: ${stationId}. Error: ${error}`
+        );
+        return cb(error)
+      }
     }
   });
 
-  // Wait for all fetch requests to complete and return array of station objects
   let stationData = await Promise.all(requests);
-  let out = formatData(stationData);
-  // out = translate(out);
-  // await translateValues(out)
-  console.log(out.length);
-  return out;
+
+  let formattedData = formatData(stationData);
+  // Filter the data and replace 'parameter' with value from 'translations'
+  let translatedData = formattedData
+    .filter((measurement) =>
+      translations.hasOwnProperty(measurement.parameter)
+    )
+    .map((measurement) => {
+      measurement.parameter = translations[measurement.parameter];
+      return measurement;
+    });
+
+  cb(null, { name: 'unused', measurements: translatedData });
 }
 
 async function fetchStations() {
   try {
-    let bearer;
-    const response = await fetch(STATIONS_URL, {
-      headers: HEADERS,
-      method: 'GET',
-      body: null,
-    });
-    const data = await response.json();
-    // console.log(data);
+    const response = await getter(STATIONS_URL);
+    const data = JSON.parse(response.body);
     return data;
   } catch (error) {
-    console.error(error);
+    log.error(error);
   }
 }
 
@@ -85,23 +93,35 @@ function formatData(stationData) {
   stationData.forEach((station) => {
     const {
       n: location,
-      o: agency,
       x: longitude,
       y: latitude,
     } = station;
 
     station.data.forEach((data) => {
+      const dt = DateTime.fromISO(data.time, { setZone: true });
+
       if (data.time && data.value) {
         const formattedMeasurement = {
           location,
-          agency,
-          longitude,
-          latitude,
-          elementName: data.elementName,
-          stationName: data.stationName,
-          time: data.time,
-          measUnit: data.measUnit,
+          city: ' ',
+          parameter: data.elementName,
           value: data.value,
+          unit: data.measUnit,
+          date: {
+            utc: dt.toUTC().toISO({ suppressMilliseconds: true }),
+            local: dt.toISO({ suppressMilliseconds: true }),
+          },
+          coordinates: {
+            longitude,
+            latitude,
+          },
+          averagingPeriod: { unit: 'hours', value: 1 },
+          attribution: [
+            {
+              name: 'arpa-basilicata',
+              url: 'https://arpabaegis.arpab.it',
+            },
+          ]
         };
         formattedData.push(formattedMeasurement);
       }
@@ -132,138 +152,20 @@ async function getBearerAuth() {
   return auth;
 }
 
-////// Print it
-fetchData()
-  .then((measurements) => {
-    console.dir(measurements, { depth: null });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-///////
-
-let translations = {
-  'Dissolved Oxygen': 'Dissolved Oxygen',
-  'Water pH': 'Water pH',
-  'Water Level': 'Water Level',
-  'Vector Wind Speed': 'Vector Wind Speed',
-  'Relative Humidity': 'Relative Humidity',
-  'idrocarburi totali non metanic': 'total non-methanic hydrocarbons',
-  'POLV.': 'DUST',
-  'Idrogeno solforato': 'Hydrogen sulphide',
-  'Temperatura H2O Retry - AT500': 'Temperatura H2O Retry - AT500',
-  'Temperatura acqua BEC': 'BEC water temperature',
-  "Velocita' del vento_val": 'Wind speed_val',
-  DMS_val: 'DMS_val',
-  'o-Xilene_val': 'o-Xylene_val',
-  'Water Temperature': 'Water Temperature',
-  'Ossigeno Disciolto': 'Dissolved Oxygen',
-  Redox: 'Redox',
-  'Clorofilla - AT500': 'Chlorophyll - AT500',
-  'Temperatura aria_val': 'Air temperature_val',
-  Toluene_val: 'Toluene_val',
-  'PM10, particolato_val': 'PM10, particulate_val',
-  'm,p-Xileni': 'm,p-Xileni',
-  'Monossido di carbonio': 'Carbon monoxide',
-  'Velocità del vento': 'Wind speed',
-  'Atmospheric Pressure': 'Atmospheric Pressure',
-  mpxilen_val: 'mpxilen_val',
-  'Profondità sonda': 'Probe depth',
-  'Scalar Wind Speed': 'Scalar Wind Speed',
-  mxilene: 'mxilene',
-  'PM2.5, materiale particolato f': 'PM2.5, particulate matter f',
-  'Sound Speed in Air': 'Sound Speed in Air',
-  'Radiazione netta_val': 'Net radiation_val',
-  'PM2.5, particolato_val': 'PM2.5, particulate_val',
-  'Temperatura aria': 'Air temperature',
-  'Air Temperature': 'Air Temperature',
-  'Pressione atmosferica_val': 'Atmospheric pressure_val',
-  'ISE Ammonio': 'ISE Ammonium',
-  'Profondità Sonda': 'Probe depth',
-  "Umidita' relativa": 'Relative humidity',
-  Toluene: 'Toluene',
-  'Elevazione del vento': 'Wind elevation',
-  'Americio 241': 'Americium 241',
-  'O3, ozono': 'O3, ozono',
-  'NO, monossido di azoto': 'NO, nitric oxide',
-  Turbidity: 'Turbidity',
-  'Scalar Wind Direction': 'Scalar Wind Direction',
-  Etilbenzene_val: 'With ethylbenzene_',
-  'Direzione del vento_val': 'Wind direction_val',
-  'Vector Wind Direction': 'Vector Wind Direction',
-  'PM10, materiale particolato fr': 'PM10, particulate material fr',
-  Benzene_val: 'Benzene_val',
-  'Monossido di carbonio_val': 'Carbon monoxide_val',
-  pH: 'pH',
-  'NOx, ossidi di azoto_val': 'NOx, nitrogen oxides_val',
-  'NO2, biossido di azoto': 'NO2, nitrogen dioxide',
-  'Direzione del vento': 'Wind direction',
-  'Pressione atmosferica': 'Atmospheric pressure',
-  Benzene: 'Benzene',
-  "Umidita' relativa_val": 'Relative humidity_val',
-  'Idrogeno solforato_val': 'Hydrogen sulphide_val',
-  'Gamma Radiation': 'Gamma Radiation',
-  'm,p-Xileni_val': 'with m,p-Xileni_',
-  'Cesio 134': 'Cesium 134',
-  Metano: 'Methane',
-  'SO2, biossido di zolfo_val': 'SO2, sulfur dioxide_val',
-  'Elevazione del vento_val': 'Wind elevation_val',
-  Conducibilità: 'conductivity',
-  BenzGC: 'BenzGC',
-  Etilbenzene: 'Etilbenzene',
-  'Temperatura sonica_val': 'Sonic temperature_val',
-  'NO2, biossido di azoto_val': 'NO2, nitrogen dioxide_val',
-  'O3, ozono_val': 'O3, ozono_val',
-  pxilene_val: 'pxilene_val',
-  MET: 'MET',
-  'Water Redox': 'Water Redox',
-  "Conducibilita' BEC": "Conducibilita' BEC",
-  'Direct Solar Radiation': 'Direct Solar Radiation',
-  'Idrocarburi non metanici_val': 'Non-methane hydrocarbons_val',
-  'NO, monossido di azoto_val': 'NO, nitric oxide_val',
-  'Cobalto 60': 'Cobalt 60',
-  'SO2, biossido di zolfo': 'SO2, sulfur dioxide',
-  'Gust Wind Speed': 'Gust Wind Speed',
-  Conductivity: 'Conductivity',
-  'NOx, ossidi di azoto': 'NOx, nitrogen oxides',
-  EtSH: 'EtSH',
-  'Berillio 7': 'Beryllium 7',
-  'Radiazione globale_val': 'Global radiation_val',
-  'Snow Level': 'Snow Level',
-  'Radiazione netta': 'net radiation',
-  'THC, idrocarburi totali': 'THC, total hydrocarbons',
-  'Gust Wind Direction': 'Gust Wind Direction',
-  'Livello Idrometrico VEGA': 'VEGA hydrometric level',
-  RAD_SOL: 'ROW_SOL',
-  'Umidità relativa': 'Relative humidity',
-  pxilene: 'pxilene',
-  mxilene_val: 'mxilene_val',
-  MeSH: 'MeSH',
-  'Temperatura acqua ISE': 'ISE water temperature',
-  "Velocita' del vento": 'Wind speed',
-  'Iodio 131': 'Iodine 131',
-  DMS: 'DMS',
-  Radon: 'Radon',
-  "Velocita' suono aria_val": 'Sound velocity air_val',
-  'Kripton 85': 'Krypton 85',
-  'o-Xilene': 'o-Xylene',
-  MeSH_val: 'MeSH_val',
-  'ISE Cloruri': 'ISE Chlorides',
-  mpxilen: 'mpxilen',
-  'Profondità Sonda BEC': 'BEC probe depth',
-  RAD_SOL_val: 'ROW_SOL_val',
-  'Cesio 137': 'Cesium 137',
-  'Temperatura sonica': 'Sonic temperature',
-  EtSH_val: 'EtSH_val',
-  'Potassio 40': 'Potassium 40',
-  'Temperatura H2O - AT500': 'Temperatura H2O - AT500',
-  'ISE Nitrati': 'ISE Nitrates',
-  'Radiazione globale': 'global radiation',
-  Metano_val: 'Methane_val',
-  MET_val: 'MET_val',
-  'pH BEC': 'pH BEC',
-  'Redox BEC': 'Redox BEC',
-  CS: 'CS',
-  'Torbidimetro BEC': 'BEC turbidimeter',
-  'Ossigeno disciolto BEC': 'Dissolved oxygen BEC',
+let translations = { // other values are from the same locations
+  'PM2.5, particolato_val': 'pm25',
+  'PM10, particolato_val': 'pm10',
+  // 'PM10, particulate material fr': 'pm10',
+  // 'Monossido di carbonio': 'co',
+  'Monossido di carbonio_val': 'co',
+  // 'SO2, biossido di zolfo': 'so2',
+  'SO2, biossido di zolfo_val': 'so2',
+  // 'NO2, biossido di azoto': 'no2',
+  'NO2, biossido di azoto_val': 'no2',
+  // 'O3, ozono': 'o3',
+  'O3, ozono_val': 'o3',
+  // 'NO, monossido di azoto': 'no',
+  'NO, monossido di azoto_val': 'no',
+  // 'NOx, ossidi di azoto': 'nox',
+  'NOx, nitrogen oxides_val': 'nox',
 };
