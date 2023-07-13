f273f44efb4485fc87c5137b146b8b3d92d4011b
Russ Biggs <russbiggs@gmail.com>
2023-07-13 10:07:44 -0600

    revert cdk

--- cdk/app.ts
+++ cdk/app.ts
@@ -2,9 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import { readFileSync } from 'fs';
 import { RealtimeFetcherStack } from './stack';
 
-///const DOTENV = process.env.DOTENV || '.env';
+const DOTENV = process.env.DOTENV || '.env';
 
-//const envs = readFileSync(`../src/${DOTENV}`, 'utf8');
+const envs = readFileSync(`../src/${DOTENV}`, 'utf8');
 
 interface keyable {
   [key: string]: string  
@@ -16,16 +16,16 @@ const env: keyable = {
 
 const reserved_keys = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY'];
 
-// envs.split('\n').forEach(function (e) {
-//   if (e) {
-//     let idx = e.indexOf('=');
-//     let name = e.substring(0,idx);
-//     let value = e.substring(idx + 1, e.length);
-//     if (!env[name] && !reserved_keys.includes(name)) {
-//       env[name] = value;
-//     }
-//   }
-// });
+envs.split('\n').forEach(function (e) {
+  if (e) {
+    let idx = e.indexOf('=');
+    let name = e.substring(0,idx);
+    let value = e.substring(idx + 1, e.length);
+    if (!env[name] && !reserved_keys.includes(name)) {
+      env[name] = value;
+    }
+  }
+});
 
 const app = new cdk.App();
 
