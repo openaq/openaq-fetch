ac6c98d3ab3712e8ac710886f6906a2520e53e4f
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2024-01-17 08:48:55 -0700

    update node to 18 in CDK stack.ts

--- cdk/stack.ts
+++ cdk/stack.ts
@@ -72,7 +72,7 @@ export class RealtimeFetcherStack extends cdk.Stack {
         code: lambda.Code.fromAsset('../src'),
         handler: 'fetch.handler',
         memorySize: 1024,
-        runtime: lambda.Runtime.NODEJS_14_X,
+        runtime: lambda.Runtime.NODEJS_18_X,
         timeout: cdk.Duration.seconds(900),
         environment: env,
       }
