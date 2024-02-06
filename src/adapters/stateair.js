38350015dc1496ae5afc22a86bfd6e0ede891658
Gabriel Fosse <67290377+majesticio@users.noreply.github.com>
2024-02-06 07:19:34 -0800

    Add Taiwan (#1075)
    * start
    
    * Taiwan is live! 85 stations
    
    * requested PR changes
    
    * update to tw.json description
    
    * Updated source to include a sourceName
    
      This keeps the source name in the db consistent with the older data
      while allowing us to use taiwan-moenv as the deployment name.
    
    ---------
    
    Co-authored-by: Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
    Co-authored-by: Christian Parker <c.andrew.parker@gmail.com>

--- src/adapters/stateair.js
+++ src/adapters/stateair.js
@@ -1,6 +1,6 @@
 /**
  * This code is responsible for implementing all methods related to fetching
- * and returning data for the SateAir data sources.
+ * and returning data for the StateAir data sources.
  */
 
 'use strict';
