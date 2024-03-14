6e787bdd559967d702c1128b75fbd32413408416
Christian Parker <c.andrew.parker@gmail.com>
2024-03-14 09:01:06 -0700

    Indentation

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -6,14 +6,14 @@ import got from 'got';
 import { FetchError, AdapterError, DATA_URL_ERROR } from './errors.js';
 
 const headers = {
-		get: { 'User-Agent': 'OpenAQ' },
-		post: {
-				'User-Agent': 'OpenAQ',
-				accept: "application/json, text/javascript, */*; q=0.01",
-				"accept-language": "en-US,en;q=0.9",
-				"cache-control": "no-cache",
-				"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-				pragma: "no-cache",
+    get: { 'User-Agent': 'OpenAQ' },
+    post: {
+        'User-Agent': 'OpenAQ',
+        accept: "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        pragma: "no-cache",
     }
 };
 
@@ -33,33 +33,33 @@ export default (source, cb, method = 'GET', params='', responseType='json') => {
   }
 
   if(typeof(params) === 'object') {
-			// convert to string
-	}
+      // convert to string
+  }
 
-	const options = {
-			method,
-			body: params,
-			responseType,
-			timeout: {
-					request: timeout,
-			},
-			retry: {
-					limit: retries,
-					errorCodes: [
-							'ETIMEDOUT'
-					],
-			},
-			headers: headers.post,
-			hooks: {
-					beforeRetry: [
-							data => {
-									log.warn(`Retrying request to ${url}`);
-							}
-					],
-			}
-	};
+  const options = {
+      method,
+      body: params,
+      responseType,
+      timeout: {
+          request: timeout,
+      },
+      retry: {
+          limit: retries,
+          errorCodes: [
+              'ETIMEDOUT'
+          ],
+      },
+      headers: headers.post,
+      hooks: {
+          beforeRetry: [
+              data => {
+                  log.warn(`Retrying request to ${url}`);
+              }
+          ],
+      }
+  };
 
-	//const opt = !!options ? options : internal_options;
+  //const opt = !!options ? options : internal_options;
 
   const requestClient = got.extend(options);
   log.debug(`Requesting response from ${options.method}: ${url}`);
@@ -69,12 +69,12 @@ export default (source, cb, method = 'GET', params='', responseType='json') => {
       // could do some checking here
       return res;
     })
-			.catch( cause => {
-				const err = new FetchError(DATA_URL_ERROR, source, 'request client error', `${cause.code}:${url}`);
+      .catch( cause => {
+        const err = new FetchError(DATA_URL_ERROR, source, 'request client error', `${cause.code}:${url}`);
       if (cb) {
         return cb({ message: err });
       } else {
-				throw err;
+        throw err;
       }
     });
 };
