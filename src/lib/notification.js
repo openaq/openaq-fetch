aa0d1cfe7adcaf9322f8c9600478d5d14157dae6
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2024-03-20 16:36:12 -0700

    formatting

--- src/lib/notification.js
+++ src/lib/notification.js
@@ -4,15 +4,15 @@ import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 const sns = new SNSClient();
 
 async function publish(message, subject) {
-		// the following just looks better in the log
-		if(process.env.TOPIC_ARN) {
-				const cmd = new PublishCommand({
-						TopicArn: process.env.TOPIC_ARN,
-						Subject: subject,
-						Message: JSON.stringify(message),
-				});
-				await sns.send(cmd);
-		}
+  // the following just looks better in the log
+  if (process.env.TOPIC_ARN) {
+    const cmd = new PublishCommand({
+      TopicArn: process.env.TOPIC_ARN,
+      Subject: subject,
+      Message: JSON.stringify(message),
+    });
+    await sns.send(cmd);
+  }
 }
 
 /**
@@ -24,33 +24,36 @@ async function publish(message, subject) {
  * @param {URL} apiURL
  * @param {String} webhookKey
  */
-export function reportAndRecordFetch (fetchReport, sources, env) {
-    return async (results) => {
-        fetchReport.results = results;
-        fetchReport.timeEnded = Date.now();
-        fetchReport.errors = results.reduce((acc, {failures}) => {
-            Object.entries(failures).forEach(([key, count]) => {
-                acc[key] = (acc[key] || 0) + count;
-            });
-            return acc;
-        }, {});
+export function reportAndRecordFetch(fetchReport, sources, env) {
+  return async (results) => {
+    fetchReport.results = results;
+    fetchReport.timeEnded = Date.now();
+    fetchReport.errors = results.reduce((acc, { failures }) => {
+      Object.entries(failures).forEach(([key, count]) => {
+        acc[key] = (acc[key] || 0) + count;
+      });
+      return acc;
+    }, {});
 
+    const failures = fetchReport.results.filter((r) => !r.count);
 
-        const failures = fetchReport.results
-              .filter(r => !r.count);
+    const successes = fetchReport.results.filter((r) => r.count > 0);
 
-        const successes = fetchReport.results
-              .filter(r => r.count > 0);
+    failures.map((r) => {
+      log.debug(r);
+    });
+    log.info(
+      `Dry run finished with ${successes.length} successes and ${
+        failures.length
+      } failures in ${
+        (fetchReport.timeEnded - fetchReport.timeStarted) / 1000
+      } seconds`
+    );
 
-        failures.map(r => {
-            log.debug(r);
-        });
-        log.info(`Dry run finished with ${successes.length} successes and ${failures.length} failures in ${(fetchReport.timeEnded - fetchReport.timeStarted)/1000} seconds`);
+    if (!env.dryrun) {
+      await publish(fetchReport.results, 'fetcher/success');
+    }
 
-        if (!env.dryrun) {
-		        await publish(fetchReport.results, 'fetcher/success');
-        }
-
-        return 0;
-    };
+    return 0;
+  };
 }
