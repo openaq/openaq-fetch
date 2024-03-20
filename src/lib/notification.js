3ebdaf29b187984396f68c5c8544e5340a4de729
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2024-03-20 16:35:27 -0700

    requested updates to notification.js

--- src/lib/notification.js
+++ src/lib/notification.js
@@ -1,6 +1,4 @@
-import request from 'request';
 import log from './logger.js';
-import { promisify } from 'util';
 import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 
 const sns = new SNSClient();
@@ -23,22 +21,36 @@ async function publish(message, subject) {
  * @param {FetchReport} fetchReport
  * @param {Source[]} sources
  * @param {Object} argv
+ * @param {URL} apiURL
+ * @param {String} webhookKey
  */
-export function reportAndRecordFetch (fetchReport, sources, argv) {
-  return async (results) => {
-    fetchReport.results = results;
-    fetchReport.timeEnded = Date.now();
-    fetchReport.errors = results.reduce((acc, {failures}) => {
-      Object.entries(failures).forEach(([key, count]) => {
-        acc[key] = (acc[key] || 0) + count;
-      });
-      return acc;
-    }, {});
+export function reportAndRecordFetch (fetchReport, sources, env) {
+    return async (results) => {
+        fetchReport.results = results;
+        fetchReport.timeEnded = Date.now();
+        fetchReport.errors = results.reduce((acc, {failures}) => {
+            Object.entries(failures).forEach(([key, count]) => {
+                acc[key] = (acc[key] || 0) + count;
+            });
+            return acc;
+        }, {});
 
-    if (argv.dryrun) {
-      log.info(fetchReport);
-      log.info('Dry run ended.');
-      return 0;
-    }
-  };
+
+        const failures = fetchReport.results
+              .filter(r => !r.count);
+
+        const successes = fetchReport.results
+              .filter(r => r.count > 0);
+
+        failures.map(r => {
+            log.debug(r);
+        });
+        log.info(`Dry run finished with ${successes.length} successes and ${failures.length} failures in ${(fetchReport.timeEnded - fetchReport.timeStarted)/1000} seconds`);
+
+        if (!env.dryrun) {
+		        await publish(fetchReport.results, 'fetcher/success');
+        }
+
+        return 0;
+    };
 }
