625a906906d143fae96598ceac55f966982e058c
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2024-03-20 08:14:52 -0700

    cleanujp aipurl and webhookjey

--- src/lib/notification.js
+++ src/lib/notification.js
@@ -2,17 +2,6 @@ import request from 'request';
 import log from './logger.js';
 import { promisify } from 'util';
 
-/**
-* Ping openaq-api to let it know cause fetching is complete
-* @param {function} cb A function of form func(cause) called on completion
-*/
-async function sendUpdatedWebhook (apiURL, webhookKey) {
-  var form = {
-    key: webhookKey,
-    action: 'DATABASE_UPDATED'
-  };
-  return promisify(request.post)(apiURL, { form: form });
-}
 
 /**
  * Reports and saves fetch information.
@@ -20,10 +9,8 @@ async function sendUpdatedWebhook (apiURL, webhookKey) {
  * @param {FetchReport} fetchReport
  * @param {Source[]} sources
  * @param {Object} argv
- * @param {URL} apiURL
- * @param {String} webhookKey
  */
-export function reportAndRecordFetch (fetchReport, sources, argv, apiURL, webhookKey) {
+export function reportAndRecordFetch (fetchReport, sources, argv) {
   return async (results) => {
     fetchReport.results = results;
     fetchReport.timeEnded = Date.now();
@@ -39,9 +26,5 @@ export function reportAndRecordFetch (fetchReport, sources, argv, apiURL, webhoo
       log.info('Dry run ended.');
       return 0;
     }
-
-    await sendUpdatedWebhook(apiURL, webhookKey);
-    log.info('Webhook posted, have a good day!');
-    return 0;
   };
 }
