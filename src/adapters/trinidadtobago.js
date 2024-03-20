fb8a5bec036eace30cfe5298c60cafce3a95068b
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2024-03-20 14:29:22 -0700

    fixed adapter, added extra stations

--- src/adapters/trinidadtobago.js
+++ src/adapters/trinidadtobago.js
@@ -6,17 +6,12 @@
  * This is a two-stage adapter requiring loading multiple urls before parsing
  * data.
  */
+
 'use strict';
 
-import {
-  unifyParameters,
-  unifyMeasurementUnits,
-} from '../lib/utils.js';
 import log from '../lib/logger.js';
-import { REQUEST_TIMEOUT } from '../lib/constants.js';
 import _ from 'lodash';
 import { DateTime } from 'luxon';
-//import got from 'got';
 import client from '../lib/requests.js';
 import tough from 'tough-cookie';
 
@@ -25,12 +20,14 @@ export const parameters = {
     'Carbon Monoxide': { name: 'co', unit: 'mg/m3', id: '3' },
     'Nitrogen Dioxide Concentrations [µg_m³]': { name: 'no2', unit: 'ug/m3', id: '1465' },
     'Ozone Concentrations [µg/m³]': { name: 'o3', unit: 'ug/m3', id: '2130' },
-    'Particulate Matter < 10µ': { name: 'pm10', unit: 'ug/m3', id: '18' },
-    'Particulate Matter < 2.5µ': { name: 'pm25', unit: 'ug/m3', id: '20' },
+    // 'Particulate Matter < 10µ': { name: 'pm10', unit: 'ug/m3', id: '18' }, // pm10 data available but not collected
+    'Particulate Matter < 2.5µ': { name: 'pm25', unit: 'ug/m3', id: '20' }, 
     'Sulfur Dioxide Concentrations [µg_m³]': { name: 'so2', unit: 'ug/m3', id: '23' }
 };
 
 const timestamp = DateTime.now().toMillis();
+// stations and IDs are referred to here but coordinates must be added manually 
+// "https://ei.weblakes.com/RTTPublic/SelectCode/ThemeGridData?aThemeName=AMBIENTSITE&aParentKey=-1&aBriefGrid=True&aSelectedKeysProviderId=&aNavView=&anEntity=50&gridId=SelectCode_AMBIENTSITE&Context_Bootstrap_Flag=true&_search=false&nd=1710968481872&rows=20&page=1&sidx=&sord=asc&ssSearchField=__ANY_COLUMN&ssSearchOper=cn&ssSearchString=&_=1710968480622"
 const stations = [
   {
     key: '16',
@@ -40,6 +37,28 @@ const stations = [
     longitude: -61.47468,
   },
   {
+    key: '18',
+    city: ' ',
+    location: 'EMA Shelter (Point Lisas - OLD)', // !!
+    latitude: 10.41603,
+    longitude: -61.47468,
+  },
+  {
+    key: '53',
+    city: ' ',
+    location: 'Mayaro AAQMS',
+    latitude: 10.28850,
+    longitude: -61.00689,
+  },
+  // no data for this station as of March 20, 2024
+  // {
+  //   key: '54',
+  //   city: '',
+  //   location: 'Toco AAQMS',
+  //   latitude: 10.41603,
+  //   longitude: -61.47468,
+  // },
+  {
     key: '19',
     city: 'Port of Spain',
     location: 'Port of Spain',
@@ -47,9 +66,9 @@ const stations = [
     longitude: -61.49406,
   },
   {
-    key: '47',
-    city: 'Scaroborough',
-    location: 'Signal Hill',
+    key: '57',
+    city: 'Tobago',
+    location: 'Scaroborough',
     latitude: 11.17455,
     longitude: -60.76007,
   },
@@ -69,10 +88,7 @@ const stations = [
   },
 ];
 
-
-
 /**
- * Fetches the data for a given source and returns an appropriate object
  * @param {object} source A valid source object
  * @param {function} cb A callback of the form cb(err, data)
  */
@@ -82,7 +98,7 @@ export async function fetchData(source, cb) {
   // List of keys for parameters used in url [3: 'CO', 1465: 'NO2', 2130: 'O3', 18: 'PM-10', 20: 'PM-2.5', 23: 'SO2']
 
     // Loops through all the stations, and then loops through all parameters IDS
-    //, and adds the requests to the tasks
+    // and adds the requests to the tasks
     const tasks = [];
     stations.map( meta => {
         Object.values(parameters).map( parameter => {
@@ -90,7 +106,6 @@ export async function fetchData(source, cb) {
                   source.url
                   .replace('$station', meta.key)
                   .replace('$parameter', parameter.id) + timestamp;
-
             const task = async () => {
                 try {
                     const values = await client({
@@ -106,12 +121,10 @@ export async function fetchData(source, cb) {
             tasks.push(task);
         });
     });
-
   try {
-
     const results = await Promise.all(tasks.map((task) => task()));
-    // Format the data
-    const data = formatData(results, cb);
+    const data = formatData(results);
+    log.debug('data:', data[0]);
     return cb(null, data);
   } catch (e) {
     return cb({ message: `Unknown adapter error - ${e.message}` });
@@ -120,98 +133,60 @@ export async function fetchData(source, cb) {
 
 
 /**
- * Given fetched data, turn it into a format our system can use.
  * @param {array} results Fetched source data and other metadata
- * @return {object} Parsed and standarized data our system can use
+ * @return {object} Parsed and standardized data our system can use
  */
-
-const formatData = function (results, cb) {
+const formatData = function (results) {
   let measurements = [];
 
-    // each item is a measurement from one station
-    // so if one part of that measurment is bad the whole item is bad
-    results.forEach((item) => {
-        try {
-            // find a match in the data to one of our parameters
-            let parameter = Object.keys(parameters).find((p) =>
-                item.values.hasOwnProperty(p)
-            );
+  results.forEach(({ meta, values }) => {
+    Object.keys(values).forEach(key => {
+      // Check if the key is one of our known parameters
+      if (parameters[key]) {
+        const parameterInfo = parameters[key];
+        const parameterData = values[key];
+        const labels = values.xlabels;
 
-            if (!parameter) {
-                throw new Error(
-                    `Could not find a valid parameter in [${Object.keys(item.values)}]. It is possible that the source names have changed.`
-                );
-            } else if (item.values[parameter].length !== item.values.xlabels.length) {
-                throw new Error(`Source data for ${parameter} does not seem to have matching labels`);
-            }
-
-            const template = {
-                city: item.meta.city,
-                location: item.meta.location,
-                parameter: parameters[parameter].name,
+        // Proceed if we have data and labels to work with
+        if (parameterData && labels && parameterData.length === labels.length) {
+          parameterData.forEach((value, index) => {
+            if (value !== null) {
+              const template = {
+                city: meta.city,
+                location: meta.location,
+                parameter: parameterInfo.name,
+                unit: parameterInfo.unit,
                 coordinates: {
-                    latitude: parseFloat(item.meta.latitude),
-                    longitude: parseFloat(item.meta.longitude),
+                  latitude: parseFloat(meta.latitude),
+                  longitude: parseFloat(meta.longitude),
                 },
-                attribution: [
-                    {
-                        name: 'Trinidad and Tobago Environmental Management Authority',
-                        url: 'https://ei.weblakes.com/RTTPublic/DshBrdAQI',
-                    },
-                ],
+                attribution: [{
+                  name: 'Trinidad and Tobago Environmental Management Authority',
+                  url: 'https://ei.weblakes.com/RTTPublic/DshBrdAQI',
+                }],
                 averagingPeriod: { unit: 'hours', value: 1 },
-                unit: parameters[parameter].unit
-            };
+              };
 
-            item.values[parameter].forEach((value, i) => {
-                if (value !== null) {
-                    let m = Object.assign({ value: value }, template);
+              let measurement = { ...template, value };
 
-                    const dateMoment = DateTime.fromFormat(
-                        item.values.xlabels[i],
-                        'yyyy-MM-dd HH',
-                        { zone: 'America/Port_of_spain' }
-                    );
-                    m.date = {
-                        utc: dateMoment
-                            .toUTC()
-                            .toISO({ suppressMilliseconds: true }),
-                        local: dateMoment.toISO({ suppressMilliseconds: true }),
-                    };
+              const dateMoment = DateTime.fromFormat(labels[index], 'yyyy-MM-dd HH-mm', { zone: 'America/Port_of_Spain' });
+              measurement.date = {
+                utc: dateMoment.toUTC().toISO({ suppressMilliseconds: true }),
+                local: dateMoment.toISO({ suppressMilliseconds: true }),
+              };
 
-                    m = unifyParameters(m);
-                    m = unifyMeasurementUnits(m);
-                    measurements.push(m);
-                }
-            });
-        } catch (err) {
-            // log the error but dont throw
-            log.warn(`Formatting error: ${err.message}`);
+              measurements.push(measurement);
+            }
+          });
+        } else {
+          log.warn(`Incomplete or inconsistent data for parameter: ${key} at station: ${meta.city}, ${meta.location}.`);
         }
+      }
     });
-
- // measurements = getLatestMeasurements(measurements);
+  });
 
   return {
     name: 'unused',
     measurements: measurements,
   };
 };
-
-
-// Could we remove this? Or if its limiting to a specific period could we do it better?
-function getLatestMeasurements(measurements) {
-  const latestMeasurements = {};
-
-  measurements.forEach((measurement) => {
-    const key = measurement.parameter + measurement.location;
-    if (
-      !latestMeasurements[key] ||
-      measurement.date.local > latestMeasurements[key].date.local
-    ) {
-      latestMeasurements[key] = measurement;
-    }
-  });
-
-  return Object.values(latestMeasurements);
-}
