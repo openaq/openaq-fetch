a61c6a7070ad2fd9df9d157b757ec35364f2258b
Christian Parker <c.andrew.parker@gmail.com>
2024-03-21 11:07:47 -0700

    First draft of new eea adapter
      Also some util methods and guides

--- src/adapters/dummy.js
+++ src/adapters/dummy.js
@@ -1,11 +1,58 @@
 import {FetchError, DATA_URL_ERROR} from '../lib/errors';
 import log from '../lib/logger';
+import client from '../lib/requests.js';
 
 function extractItemType (item) {
   return Object.getPrototypeOf(item).name || typeof item;
 }
 
-export const name = 'Dummy';
+// export the name of the adapter
+// all lowercase, dashes, numbers and no spaces
+export const name = 'example';
+// provide a parameter map that switches from theirs to ours
+// feel free to use this object to do more (e.g. trinidadtobago)
+// but it should at the least match their parameter to our name and units
+export const parameters = {
+    'CO': { name: 'co', unit: 'mg/m3' },
+    'NO2': { name: 'no2', unit: 'ug/m3' },
+    'O3': { name: 'o3', unit: 'ug/m3' },
+    'PM10': { name: 'pm10', unit: 'ug/m3' },
+    'PM25': { name: 'pm25', unit: 'ug/m3' },
+    'SO2': { name: 'so2', unit: 'ug/m3' }
+};
+// Include any urls at the top
+const url = "https://example.com";
+
+// split the code into basic tasks
+
+// fetchData
+// main function that is exported to get the data
+// loop through that data and pass each row for formatting
+// return to collector
+
+
+// fetchStations (optional)
+// if something is needed to get stations we start there
+
+// fetchMeasurements (probably required)
+// We should have a function that returns the measurement data
+// If it only takes one call to get all the data (for all stations and parameters)
+// Then I would expect a function that made that call and then looped through
+// each measurement and passed that data to the format data method
+// If it takes one call per station (all parameters together)
+// I would explect a loop through stations at a higher level (e.g. fetchData)
+// that calls the fetchMeasurements
+
+// formatData
+// for each row of data (not for the whole data object)
+// update parameter name
+// check for errors
+
+
+
+
+
+
 export function fetchStream (source) {
   if (source.data) {
     if (!Array.isArray(source.data)) throw source.data;
--- src/lib/requests.js
+++ src/lib/requests.js
@@ -4,17 +4,17 @@ import log from './logger.js';
 import { REQUEST_TIMEOUT, REQUEST_RETRIES } from './constants.js';
 import got from 'got';
 import { FetchError, AdapterError, DATA_URL_ERROR } from './errors.js';
+import { parse } from 'csv-parse/sync';
+import { load } from 'cheerio';
+
 
 const DEFAULT_HEADERS = {
-    get: { 'User-Agent': 'OpenAQ' },
-    get: {
-        'User-Agent': 'OpenAQ',
-        accept: "application/json, text/javascript, */*; q=0.01",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "no-cache",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        pragma: "no-cache",
-    }
+    'User-Agent': 'OpenAQ',
+    accept: "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    pragma: "no-cache",
 };
 
 export default ({
@@ -25,13 +25,44 @@ export default ({
 		retries = REQUEST_RETRIES,
 		method = 'GET',
 		responseType = 'json',
+    as,  // new argument that will replace responseType, eventually defaulted to json
 		https = {},
-    cookieJar = null,
+    csvOptions = { trim: true, comment: '#', skip_empty_lines: true, columns: true },
+    xmlOptions = { xmlMode: true },
+    htmlOptions = { },
+    cookieJar,
 }) => {
 
-		let body, requestClient;
+		let body, requestClient, toData = toSame;
 		if (!url) throw new Error('No url passed to request client');
 
+    // How would we like the data returned?
+    // each method should also include an options arg above
+    // e.g. csv has a csvOptions
+    if(as) {
+        switch (as.toLowerCase()) {
+        case 'csv':
+            responseType = 'text';
+            toData = (b) => toCSV(b, csvOptions);
+            break;
+        case 'xml':
+            responseType = 'text';
+            toData = (b) => toXML(b, xmlOptions);
+            break;
+        case 'html':
+            responseType = 'text';
+            toData = (b) => toHTML(b, htmlOptions);
+            break;
+        case 'text':
+            responseType = 'text';
+            toData = toSame;
+            break;
+        case 'json':
+            responseType = 'json';
+            toData = toSame;
+            break;
+        }
+    }
 
 		if(params && typeof(params) === 'object') {
 				// convert to string
@@ -46,8 +77,9 @@ export default ({
 		}
 
 		// if we have not passed any headers than use the default
+    // reverting to using the same default headers for all methods
 		if(!headers) {
-				headers = DEFAULT_HEADERS[method.toLowerCase()];
+				headers = DEFAULT_HEADERS;
 		} else {
 				// otherwise make sure we are passing a user agent
 				headers['User-Agent'] = 'OpenAQ';
@@ -98,7 +130,7 @@ export default ({
 								if(!res.body) {
 										throw new Error('Request was successful but did not contain a body.');
 								}
-								return res.body;
+								return toData(res.body);
 						} else if (res.statusCode == 403) {
 								throw new Error('Server responsed with forbidden (403).');
 						} else {
@@ -106,3 +138,21 @@ export default ({
 						}
 				});
 };
+
+function toSame(body) {
+    return body;
+}
+
+function toCSV(body, options) {
+    return parse(body, options);
+}
+
+function toXML(body, options) {
+    // force xmlMode
+    options.xmlMode = true;
+    return load(body, options);
+}
+
+function toHTML(body, options) {
+    return load(body, options);
+}
